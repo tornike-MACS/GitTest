5a3353b1c3de5a34e3bfa704752687260dde403c
S <s>
2023-10-02 22:18:39 +0400

    {"types":"C","runs":1}

--- Problem01.java
+++ Problem01.java
@@ -1,54 +1,7 @@
-//7. კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას მთელი სამყარო თითო ბრილიანტით. ანუ მთელ
-//სამყაროში ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყისი
-//სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
-//ბრილიანტი დევს. ამასთან გაითვალისწინეთ, რომ სამყაროს ზომები თქვენთვის უცნობია და
-//თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
-import stanford.karel.Karel;
-public class Problem01 extends Karel{
+
+import stanford.karel.SuperKarel;
+public class Problem01 extends SuperKarel{
 	public void run(){
-		if (noBeepersPresent()){
-			putBeeper();
-		}
-		while(frontIsClear()){
-		while(frontIsClear()){
-			move();
-			if (noBeepersPresent()){
-				putBeeper();
-			}
-		}
-		if (frontIsBlocked()){
-			turnLeft();
-			}
-		if(frontIsClear()){
-			move();
-			}
-			if (noBeepersPresent()){
-				putBeeper();
-			}
-			turnLeft();
-			while(frontIsClear()){
-				move();
-				if (noBeepersPresent()){
-					putBeeper();
-				}
-			}
-			if (frontIsBlocked()){
-				turnRight();
-			}
-			if (frontIsClear()){
-				move();
-			}
-			if (noBeepersPresent()){
-				putBeeper();
-			}
-			if(frontIsClear()){
-				turnRight();
-			}
-	}
-	}
-	private void turnRight(){
-		turnLeft();
-		turnLeft();
-		turnLeft();
+		
 	}
 }
