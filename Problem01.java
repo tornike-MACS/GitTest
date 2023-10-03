2d020a0f419fad98d55012ba3b50ebf8fc500431
S <s>
2023-10-03 11:59:21 +0400

    {"types":"C","runs":10}

--- Problem01.java
+++ Problem01.java
@@ -1,52 +1,10 @@
-//7. კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას მთელი სამყარო თითო ბრილიანტით. ანუ მთელ
-//სამყაროში ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყისი
-//სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
-//ბრილიანტი დევს. ამასთან გაითვალისწინეთ, რომ სამყაროს ზომები თქვენთვის უცნობია და
-//თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
+//8. კარელმა უნდა გამოაკლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+//m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n-m ბრილიანტი. ჩათვალეთ, რომ
+//კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს და ჩათვალეთ, რომ n>=m.
 import stanford.karel.SuperKarel;
 public class Problem01 extends SuperKarel{
 	public void run(){
-		while(frontIsClear()){
-			xazisShevseba();
-			if(leftIsClear()){
-				asvla1();
-				}
-			
-			xazisShevseba();
-				if(rightIsClear()){
-					asvla2();
-				}
-				}
 	
-		}
-	
-
-	private void xazisShevseba(){
-		if(noBeepersPresent()){
-			putBeeper();
-		}
-		while(frontIsClear()){
-			move();
-			if(noBeepersPresent()){
-				putBeeper();
-			}
-		}
-	}
-	private void asvla1(){
-		turnLeft();
-		move();
-		if(noBeepersPresent()){
-			putBeeper();
-		}
-		turnLeft();
-	}
-	private void asvla2(){
-		turnRight();
-		move();
-		if(noBeepersPresent()){
-			putBeeper();
-		}
-		turnRight();
 	}
 }
 
