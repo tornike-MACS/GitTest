cf6557def2e56a9cbf0ab631a42ac319ed30938f
S <s>
2023-10-09 12:43:37 +0400

    {"types":"C","runs":17}

--- Ass1_p3.java
+++ Ass1_p3.java
@@ -4,6 +4,19 @@ public class Ass1_p3 extends SuperKarel {
 
 	
 	public void run() {
-	
+		fillLine1();
+	//	moveToNextLine();
+	}
+
+	private void fillLine1() {
+		putBeeper();
+		move();
+		while(frontIsClear()){
+			move();
+			putBeeper();
+			if(frontIsClear()){
+				move();
+			}
+		}
 	}
 }
