0ef3651ca7583e2cab9fb0c43e5021c7fccdfe8d
S <s>
2023-10-09 12:04:47 +0400

    {"types":"C","runs":10}

--- Ass1_p2.java
+++ Ass1_p2.java
@@ -5,7 +5,16 @@ public class Ass1_p2 extends SuperKarel {
 	
 	public void run() {
 		fixColumn();
+		returnToFirstPos();
 //		moveToNextColumn();
+	}
+
+	private void returnToFirstPos() {
+		turnAround();
+		while(frontIsClear()){
+			move();
+		}
+		turnLeft();
 	}
 
 	private void fixColumn() {
