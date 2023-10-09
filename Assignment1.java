eb086b67242b67a687108e837943ffea18b75c38
S <s>
2023-10-09 11:33:23 +0400

    {"types":"C","runs":1}

--- Assignment1.java
+++ Assignment1.java
@@ -1,12 +1,29 @@
 import stanford.karel.Karel;
 
 public class Assignment1 extends Karel {
-
-	
 	public void run() {
-		move();
-		move();
-		move();
+		goOut();
+		pickBeeper();
+		goIn();
+	}
 
+	private void goOut() {
+		moveThreeTimes();
+		turnRight();
+		move();
+		turnLeft();
+		move();
 	}
+
+	private void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
 	}
+
+	private void moveThreeTimes() {
+		move();
+		move();
+		move();
+	}
+}
