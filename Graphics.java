37d591668fc3be98fa658cbe9acab657411556eb
S <s>
2023-10-10 17:24:12 +0400

    {"types":"C","runs":4}

--- Graphics.java
+++ Graphics.java
@@ -3,7 +3,6 @@ import acm.program.GraphicsProgram;
 
 public class Graphics extends GraphicsProgram {
 	public void run() {
-		add(new GOval(100, 200));
-		
+		add(new GOval(100, 200), 100, 100);
 	}
 }
