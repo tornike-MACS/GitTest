1454481b56e463c76d8c0add512c64fe5ea221b8
S <s>
2023-10-12 09:06:59 +0400

    {"types":"C","runs":25}

--- Problem13.java
+++ Problem13.java
@@ -1,10 +1,12 @@
 import acm.graphics.GLine;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+import java.awt.*;
 
 public class Problem13 extends GraphicsProgram {
 	public void run() {
 		GLine myLine = new GLine(0, 230, 1000, 230);
+		myLine.setColor(Color.RED);
 		add(myLine);
 	}
 }
