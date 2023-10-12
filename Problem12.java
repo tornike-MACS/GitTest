c1fcb1d9382d89359e35807a275e52b16de564a6
S <s>
2023-10-12 08:43:47 +0400

    {"types":"C","runs":19}

--- Problem12.java
+++ Problem12.java
@@ -1,4 +1,20 @@
+import acm.graphics.GLine;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem12 {
-
+public class Problem12 extends GraphicsProgram {
+	public void run() {
+		GRect myRect = new GRect (400, 200);
+		add(myRect, 180, 200);
+		GLine myLine = new GLine (180, 200, 380, 100);
+		add(myLine);
+		myLine = new GLine (380, 100, 580, 200);
+		add(myLine);
+		GRect window = new GRect(80, 80);
+		add(window, 220, 240);
+		window = new GRect(80, 80);
+		add(window, 460, 240);
+		GRect door = new GRect(60, 120);
+		add(door, 350, 280 );
+	}
 }
