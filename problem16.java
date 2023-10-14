1fa81f3b64365910d5fafe03e667ce8743f27dc8
S <s>
2023-10-14 15:37:29 +0400

    {"types":"C","runs":7}

--- problem16.java
+++ problem16.java
@@ -1,4 +1,10 @@
+import acm.program.*;
 
-public class problem16 {
-
+public class problem16 extends ConsoleProgram{
+	public void run(){
+		double n1 = readDouble("first number: ");
+		double n2 = readDouble("second number: ");
+		double n3 = readDouble("third number: ");
+		println("The answer is: " + 1/(1/n1 + 1/n2 + 1/n3));
+	}
 }
