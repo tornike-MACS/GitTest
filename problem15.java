d34bac4f4aece7046edd8699c7fe78a243408b3e
S <s>
2023-10-14 15:32:48 +0400

    {"types":"C","runs":6}

--- problem15.java
+++ problem15.java
@@ -3,6 +3,6 @@ public class problem15 extends ConsoleProgram {
 	public void run() {
 		double n1 = readDouble("first number:");
 		double n2 = readDouble("second number:");
-		println((n1 + n2)/2);
+		println("The avarage is: " + (n1 + n2)/2);
 	}
 }
