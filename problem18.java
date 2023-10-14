2948738a0b931e346c687bc1cefb51b2788e563a
S <s>
2023-10-14 16:48:27 +0400

    {"types":"C","runs":20}

--- problem18.java
+++ problem18.java
@@ -5,9 +5,9 @@ public class problem18 extends ConsoleProgram {
 		int n1 = readInt("first number: ");
 		int n2 = readInt("Second number: ");
 		if(n1>n2){
-			println(n1);
+			println(n1 + " is the biggest");
 		}else{
-			println(n2);
+			println(n2 + " is the biggest");
 		}
 	}
 }
