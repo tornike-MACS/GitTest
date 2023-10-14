957932fed2d161b1449591512c64ad00fa162af4
S <s>
2023-10-14 16:22:46 +0400

    {"types":"C","runs":17}

--- Problem13.java
+++ Problem13.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Problem13 extends GraphicsProgram {
 	public void run() {
-		int height = getHeight();
+		int height = getHeight()/2;
 		GLine myLine = new GLine(0, height, 1000, height);
 		myLine.setColor(Color.RED);
 		add(myLine);
