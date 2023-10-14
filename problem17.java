b30b2b2cb54c5e173c82880beab19e6762abcdda
S <s>
2023-10-14 16:15:31 +0400

    {"types":"C","runs":13}

--- problem17.java
+++ problem17.java
@@ -4,7 +4,8 @@ public class problem17 extends ConsoleProgram {
 	public void run(){
 		double n1 = readDouble("Enter your number: ");
 		int n2 = (int) n1;
+		double n3 = n1 - n2; 
 		
-		println(n2 +  "and");
+		println(n2 +  " and " + n3);
 	}
 }
