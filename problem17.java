623dc6fe5259e6916439d40ac9466c5b8e1257ba
S <s>
2023-10-14 15:58:34 +0400

    {"types":"C","runs":10}

--- problem17.java
+++ problem17.java
@@ -1,4 +1,10 @@
+import acm.program.*;
 
-public class problem17 {
-
+public class problem17 extends ConsoleProgram {
+	public void run(){
+		double n1 = readDouble("Enter your number: ");
+		double n2 = n1 + 1; 
+		
+		println(n1%n2 + "and" + n1 - n1%n2);
+	}
 }
