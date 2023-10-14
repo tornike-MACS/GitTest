0148e700c4de1555d6e64fa1ddbae236238ff09f
S <s>
2023-10-14 22:10:42 +0400

    {"types":"C","runs":3}

--- problem14.java
+++ problem14.java
@@ -4,10 +4,10 @@ import java.awt.*;
 
 public class problem14 extends GraphicsProgram {
 	public void run(){
-		GRect head = new GRect(300, 400);
+		GRect head = new GRect(200, 300);
 		head.setFilled(true);
 		head.setColor(Color.RED);
-		add(head, 200, 200);
+		add(head, 100, 100);
 		
 	}
 }
