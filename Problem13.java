32b04e130218e19d55a51f6f8f7c61eadf1444b1
S <s>
2023-10-14 16:22:20 +0400

    {"types":"C","runs":16}

--- Problem13.java
+++ Problem13.java
@@ -5,7 +5,8 @@ import java.awt.*;
 
 public class Problem13 extends GraphicsProgram {
 	public void run() {
-		GLine myLine = new GLine(0, 230, 1000, 230);
+		int height = getHeight();
+		GLine myLine = new GLine(0, height, 1000, height);
 		myLine.setColor(Color.RED);
 		add(myLine);
 	}
--- problem17.java
+++ problem17.java
@@ -5,7 +5,6 @@ public class problem17 extends ConsoleProgram {
 		double n1 = readDouble("Enter your number: ");
 		int n2 = (int) n1;
 		double n3 = n1 - n2; 
-		
 		println(n2 +  " and " + n3);
 	}
 }
