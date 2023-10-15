cd092892513b3f12ad46f1085733b8da02eb4c32
S <s>
2023-10-15 13:06:47 +0400

    {"types":"A","runs":10}

---
+++ Graphics.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Graphics extends GraphicsProgram {
+	public void run() {
+//		GOval myOval = new GOval(100, 200);
+//		add(myOval, 100, 100);
+//		GOval newOval = new GOval(100, 200);
+//		add(newOval, 200, 200);
+//		newOval.setFilled(true);
+//		newOval.setColor(Color.RED);
+		GRect myRect = new GRect (400, 200);
+		add(myRect, 180, 200);
+		GLine myLine = new GLine (180, 200, 380, 100);
+		add(myLine);
+		myLine = new GLine (380, 100, 580, 200);
+		add(myLine);
+		GRect window = new GRect(80, 80);
+		add(window, 220, 240);
+		window = new GRect(80, 80);
+		add(window, 460, 240);
+		GRect door = new GRect(60, 120);
+		add(door, 350, 280 );
+	}
+}
