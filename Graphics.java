4b067d4d24b19a8aec53801ab4d7b0c69cd5035f
S <s>
2023-10-17 11:43:06 +0400

    {"types":"C","runs":6}

--- Graphics.java
+++ Graphics.java
@@ -8,6 +8,14 @@ public class Graphics extends GraphicsProgram {
 		GRect Rect = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
 		Rect.setFilled(true);
 		add(Rect);
+		double dx = (getWidth() - SQUARE_SIZE)/N_STEPS;
+		double dy = (getHeight() - SQUARE_SIZE)/N_STEPS;
+		for(int i = 0; i < N_STEPS; i++){
+			Rect.move(dx, dy);
+			pause(PAUSE_TIME);
+		}
 	}
 	private static final int SQUARE_SIZE = 50;
+	private static final int N_STEPS = 1000;
+	private static final int PAUSE_TIME = 20;
 }
