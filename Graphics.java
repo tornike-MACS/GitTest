f3921cc289d7330dc4ce671a4faf0255ab3bf7ff
S <s>
2023-10-17 11:46:21 +0400

    {"types":"C","runs":8}

--- Graphics.java
+++ Graphics.java
@@ -5,13 +5,13 @@ import acm.program.*;
 
 public class Graphics extends GraphicsProgram {
 	public void run() {
-		GRect Rect = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
-		Rect.setFilled(true);
+		GRect rect = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+		rect.setFilled(true);
 		add(Rect);
 		double dx = (getWidth() - SQUARE_SIZE)/N_STEPS;
 		double dy = (getHeight() - SQUARE_SIZE)/N_STEPS;
 		for(int i = 0; i < N_STEPS; i++){
-			Rect.move(dx, dy);
+			rect.move(dx, dy);
 			pause(PAUSE_TIME);
 		}
 	}
