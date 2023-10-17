13f484b2673599365cdfae61bd84d82669a1aff8
S <s>
2023-10-17 11:49:36 +0400

    {"types":"C","runs":12}

--- Graphics.java
+++ Graphics.java
@@ -12,10 +12,10 @@ public class Graphics extends GraphicsProgram {
 		double dy = (getHeight() - SQUARE_SIZE)/N_STEPS;
 		for(int i = 0; i < N_STEPS; i++){
 			rect.move(dx, dy);
-//			pause(PAUSE_TIME);
+			pause(PAUSE_TIME);
 		}
 	}
 	private static final int SQUARE_SIZE = 50;
-	private static final int N_STEPS = 1000;
+	private static final int N_STEPS = 100;
 	private static final int PAUSE_TIME = 20;
 }
