7ae1dab9b55aebb26521099965c5bd8db365b21d
S <s>
2023-10-17 11:37:02 +0400

    {"types":"C","runs":5}

--- Graphics.java
+++ Graphics.java
@@ -5,9 +5,9 @@ import acm.program.GraphicsProgram;
 
 public class Graphics extends GraphicsProgram {
 	public void run() {
-		GRect myRect = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
-		myRect.setFilled(true);
-		
+		GRect Rect = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+		Rect.setFilled(true);
+		add(Rect);
 	}
 	private static final int SQUARE_SIZE = 50;
 }
