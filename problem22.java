402313dd19d6dfcabed8fc98c83b1112d810db73
S <s>
2023-10-18 11:15:30 +0400

    {"types":"C","runs":24}

--- problem22.java
+++ problem22.java
@@ -9,7 +9,7 @@ public class problem22 extends GraphicsProgram{
 		GRect square = new GRect(10, 10);
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
-				add(square, x, y);
+				add(square);
 				square.setFilled((i+j) % 2 != 0);
 				x += sizeOfSquare;
 			}
