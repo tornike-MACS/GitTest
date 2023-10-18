71bbe2c30e1b5dfaa4f5158adfedffbec2a6f20d
S <s>
2023-10-18 11:21:03 +0400

    {"types":"C","runs":32}

--- problem22.java
+++ problem22.java
@@ -3,17 +3,17 @@ import acm.program.*;
 
 public class problem22 extends GraphicsProgram{
 	public void run(){
-		int y = 0;
+		double y = 0;
 		double x = 0;
-		double sizeOfSquare = getHeight()/SIZE_OF_TABLE;
+		double sizeOfSquare = (double)getHeight()/SIZE_OF_TABLE;
 		GRect square = new GRect(y, x, sizeOfSquare, sizeOfSquare);
-		for(int i = 0; i < 8; i++){
-			for(int j = 0; j < 8; j++){
+		for(int i = 0; i < SIZE_OF_TABLE; i++){
+			for(int j = 0; j < SIZE_OF_TABLE; j++){
 				add(square);
 				square.setFilled((i+j) % 2 != 0);
 				x = x + sizeOfSquare;
 			}
-			y += sizeOfSquare;
+			y = y + sizeOfSquare;
 		}
 	}
 	private static final int SIZE_OF_TABLE = 8;
