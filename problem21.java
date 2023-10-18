c51fc2207845abea203f0db39b7db702d0453ec7
S <s>
2023-10-18 20:07:27 +0400

    {"types":"C","runs":10}

--- problem21.java
+++ problem21.java
@@ -5,7 +5,7 @@ public class problem21 extends GraphicsProgram{
 	public void run(){
 		int h = getHeight();
 		for(int i = 0; i < 11; i++){
-		GLine hLine = new GLine(0, i * h / 10, 0, i * h / 10);
+		GLine hLine = new GLine(0, i * h / 10, h, i * h / 10);
 		add(hLine);
 		}
 //		double x = getHeight();
