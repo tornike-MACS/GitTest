9c6fa981f026aab6863f88620982d83e029cf046
S <s>
2023-10-18 20:11:23 +0400

    {"types":"C","runs":14}

--- problem21.java
+++ problem21.java
@@ -4,13 +4,14 @@ import acm.program.*;
 public class problem21 extends GraphicsProgram{
 	public void run(){
 		int h = getHeight();
-		for(int i = 0; i < 11; i++){
-		GLine hLine = new GLine(0, i * h / 10, h, i * h / 10);
+		for(int i = 0; i < RESOLUTION + 1; i++){
+		GLine hLine = new GLine(0, i * h / RESOLUTION, h, i * h / RESOLUTION);
 		add(hLine);
 		}
-		for(int j = 0; j < 11; j++){
-		GLine vLine = new GLine (j * h / 10, 0, j * h / 10, h);
+		for(int j = 0; j < RESOLUTION + 1; j++){
+		GLine vLine = new GLine (j * h / RESOLUTION, 0, j * h / RESOLUTION, h);
 		add(vLine);
 		}
 	}
+	private static final int RESOLUTION = 10; 
 }
