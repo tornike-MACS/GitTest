04bd17ab4bec13c481bf3c04be383ce197a238ff
S <s>
2023-10-19 17:37:03 +0400

    {"types":"C","runs":10}

--- bonusprob8_1.java
+++ bonusprob8_1.java
@@ -1,7 +1,11 @@
-import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
 
 public class bonusprob8_1 extends GraphicsProgram {
 	public void run() {
-		
+		int x = getHeight();
+		int y = x;
+		GLabel label = new GLabel("width: " + x + ", height: " + y,100, 100);
+		add(label);
 	}
 }
