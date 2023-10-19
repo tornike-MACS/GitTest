2d2941d26212e59fa31c15578bbec5c4d2e86233
S <s>
2023-10-19 16:48:01 +0400

    {"types":"C","runs":4}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -4,10 +4,15 @@ import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-		println(celsiousToFahrenheit(5) + "fahrenheit");
-		
+		println("This program creates the table of celsious to fahrenheit convention ");
+		for(int c = FROM; c<= TO; c += STEP) {
+			println(c + "C = " + celsiousToFahrenheit(c) + "F")
+		}
 	}
 	private double celsiousToFahrenheit(double c){
 		return 9.0 / 5.0 * c + 32;
 	}
+	private static final int FROM = 0;
+	private static final int TO = 100;
+	private static final int STEP = 5;
 }
