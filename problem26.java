a6ed45ec5fd8f3860376a12e90df1455af6be8d3
S <s>
2023-10-21 17:26:41 +0400

    {"types":"C","runs":14}

--- problem26.java
+++ problem26.java
@@ -1,4 +1,16 @@
+import acm.program.*;
 
-public class problem26 {
-
+//მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+//უდიდესი საერთო გამყოფი
+public class problem26 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter first number: ");
+		int b = readInt("Enter second number: ");
+		for(int i = a; i > 0; i--) {
+			if(b % i == 0 && a % i == 0) {
+				println(i);
+				break;
+			}
+		}
+	}
 }
