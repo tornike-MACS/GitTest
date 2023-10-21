1329d70d999daaf8fed741cb87719fd9d7e5a45d
S <s>
2023-10-21 17:15:40 +0400

    {"types":"C","runs":8}

--- problem25.java
+++ problem25.java
@@ -1,4 +1,16 @@
+import acm.program.*;
 
-public class problem25 {
-
+//მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+//უმცირესი საერთო ჯერადი.
+public class problem25 extends ConsoleProgram {
+	public void run() {
+		int a = readInt ("Enter first number:" );
+		int b = readInt ("Enter second number: ");
+		for(int i = a; true; i += a){
+			if(i % b == 0){
+				println(i);
+				break;
+			}
+		}
+	}
 }
