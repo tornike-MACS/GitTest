74bd4e53504eec2743530c69cacc341770b58e09
S <s>
2023-10-24 11:18:02 +0400

    {"types":"C","runs":1}

--- problem27.java
+++ problem27.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class problem27 {
-
+//ომხმარებელს შეყავს n და m დაბეჭდეთ n^m
+public class problem27 extends ConsoleProgram {
+	public void run() {
+		int answer = 1;
+		int n = readInt("Enter first number: ");
+		int m = readInt("Enter second number: ");
+		for(int i = 0; i < m; i++) {
+			answer *= n;
+		}
+		println(answer);
+	}
 }
