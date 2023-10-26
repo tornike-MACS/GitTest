112386cb71387c567546cedff9c93156a44b8c2c
S <s>
2023-10-26 21:46:28 +0400

    {"types":"C","runs":10}

--- problem30.java
+++ problem30.java
@@ -7,14 +7,15 @@ public class problem30 extends ConsoleProgram {
 		int answer = 0;
 		int n = readInt();
 		int digits = countDigits(n);
-		int s = n;
+//		int s = n;
 		for(int i = digits; i > 0; i--) {
-			int a = s % 10;
+			int a = n % 10;
 			answer += (int)Math.pow(10,i - 1) * a;
-			s = s / 10;
+			n = n / 10;
 		}
 		println(answer);
 	}
+	
 	private int countDigits(int n) {
 		int digits = 0;
 		while(n > 0) {
