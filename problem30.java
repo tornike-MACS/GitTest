16bf3cf2bf9b86044a642016de26e14ad3a15cba
S <s>
2023-10-26 09:26:57 +0400

    {"types":"C","runs":31}

--- problem30.java
+++ problem30.java
@@ -6,12 +6,13 @@ public class problem30 extends ConsoleProgram {
 	public void run() {
 		int digits = 0;
 		int n = readInt();
+		int s = n;
 		while(n > 0) {
 			n = n / 10;
 			digits++;
 		}
 		println(digits);
-		println(n);
+		println(s);
 //		for(int i = 0; i < digits; i++) {
 //			
 //		}
