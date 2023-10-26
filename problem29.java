91cf8013117a291723a5f90a01a1f222a64b6cc5
S <s>
2023-10-26 09:05:28 +0400

    {"types":"C","runs":11}

--- problem29.java
+++ problem29.java
@@ -7,6 +7,12 @@ public class problem29 extends ConsoleProgram {
 		int a = 1;
 		int b = 2;
 		b = a + b;
+		if(n == 1) {
+			println("1");
+		}
+		if(n == 2) {
+			println("2");
+		}
 		for(int i = 3; i < n; i++) {
 			a = b - a;
 			b = a + b;
