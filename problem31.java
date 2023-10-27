d378a5160dc7a851a9d2fcae85e985c270d182a3
S <s>
2023-10-27 20:34:12 +0400

    {"types":"C","runs":13}

--- problem31.java
+++ problem31.java
@@ -1,10 +1,21 @@
 import acm.program.*;
 //31. დაწერეთ პროგრამა რომელიც დაადგენს შეყვანილი რიცხვი მარტივია თუ არა
-public class problem31 extends ConsoleProgram {
+public class problem31 extends ConsoleProgram {	
 	public void run() {
 		int n = readInt();
+		if(primeOrNot(n)) {
+			println("This number is prime");
+		}else {
+			println("This number is not prime");
+		}
 	}
-	private boolean primeORNot(int n) {
-		
+	private boolean primeOrNot(int n) {
+		int divisors = 0;
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				divisors++;
+			}
+		}
+		return divisors == 2;
 	}
 }
