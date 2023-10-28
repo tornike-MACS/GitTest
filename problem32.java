a0021cd49e7d8b06bdade2484e8c611b9b49151e
S <s>
2023-10-28 15:16:51 +0400

    {"types":"C","runs":72}

--- problem32.java
+++ problem32.java
@@ -3,13 +3,13 @@ import acm.program.ConsoleProgram;
 //დაწერეთ პროგრამა რომელიც იპოვის და დაბეჭდავს 1000000 მდე ყველა მერტივ რიცხვს
 public class problem32 extends ConsoleProgram {
 	public void run() {
-		for(int i = 2; i <= 10000; i++) {
-			if(primeOrNot(i) == 2) {
+		for(int i = 2; i <= 100000; i++) {
+			if(numberOfDivisors(i) == 2) {
 				println(i);
 			}
 		}
 	}
-	private int primeOrNot(int i) {
+	private int numberOfDivisors(int i) {
 		int divisors = 0;
 		for(int j = 1; j <= i; j++) {
 			if(i % j == 0) {
