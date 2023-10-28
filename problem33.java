8c40083c416bfc232d572a3c4e3a623509153b82
S <s>
2023-10-28 22:39:29 +0400

    {"types":"C","runs":72}

--- problem33.java
+++ problem33.java
@@ -1,4 +1,7 @@
+import acm.program.GraphicsProgram;
 
-public class problem33 {
-
+//33. დაწერეთ პროგრამა რომელიც კანვასზე დახატავს შემთხვევითი ფერის მქონე წრეწირს,
+//რაიდიუსიც შემთხვევით აირჩეით შეზღუდული რეინჯიდან
+public class problem33  extends GraphicsProgram{
+	
 }
