42bc3c22351b4bee646c10539c8a9f29717886f0
S <s>
2023-10-29 18:21:58 +0400

    {"types":"C","runs":29}

--- problem34.java
+++ problem34.java
@@ -9,7 +9,7 @@ import acm.util.*;
 
 public class problem34 extends GraphicsProgram {
 	public void run() {
-		for(int i = 0; i < 1000; i++) {
+		for(int i = 0; i < 100; i++) {
 			int r = rgen.nextInt();
 			int x = rgen.nextInt();
 			int y = rgen.nextInt();
