177262881791b167f58f305a38d5426f36056aea
S <s>
2023-10-29 21:06:40 +0400

    {"types":"C","runs":12}

--- bonusprob8_3.java
+++ bonusprob8_3.java
@@ -8,6 +8,9 @@ public class bonusprob8_3 extends ConsoleProgram {
 	private int fibonachi(int n) {
 		int a = 1;
 		int b = 1;
+		if(n == 1) {
+			return 1;
+		}
 		int answer = 0;
 		for(int i = 2; i <= n; i++) {
 			answer = a + b;
