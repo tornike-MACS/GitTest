e7fdddb5893ce1536c99318be0e6d11ab05d2ebd
S <s>
2023-10-29 18:41:39 +0400

    {"types":"C","runs":40}

--- problem35.java
+++ problem35.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
+import acm.util.*;
 
-public class problem35 {
+//35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო
 
+public class problem35 extends ConsoleProgram {
+	public void run() {
+		int count = 0;
+		while(true) {
+			boolean b = rgen.nextBoolean();
+			if(b) {
+				println("borjgalo");
+				count++;
+				break;
+			}else {
+				println("safasuri");
+				count++;
+			}
+		}
+		println("we need " + count + " tosses to get a tail");
+	}
+	RandomGenerator rgen = new RandomGenerator();
 }
