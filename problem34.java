9704e5436aac4678b965da568a6040d748b702a4
S <s>
2023-10-29 18:21:28 +0400

    {"types":"C","runs":28}

--- problem34.java
+++ problem34.java
@@ -1,4 +1,25 @@
+import java.awt.Color;
 
-public class problem34 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.*;
 
+//34. კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული
+//წრე, შემთხვევით კოორდინატებზე.
+
+public class problem34 extends GraphicsProgram {
+	public void run() {
+		for(int i = 0; i < 1000; i++) {
+			int r = rgen.nextInt();
+			int x = rgen.nextInt();
+			int y = rgen.nextInt();
+			Color color = rgen.nextColor();
+			GOval circle = new GOval(x, y, r, r);
+			circle.setFilled(true);
+			circle.setColor(color);
+			add(circle);
+		}
+		
+	}
+	RandomGenerator rgen = new RandomGenerator();
 }
