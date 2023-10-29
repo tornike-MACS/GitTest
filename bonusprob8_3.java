733c463a1dde665bafc549f6d6fb7859a361a4af
S <s>
2023-10-29 21:05:25 +0400

    {"types":"C","runs":5}

--- bonusprob8_3.java
+++ bonusprob8_3.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class bonusprob8_3 {
-
+public class bonusprob8_3 extends ConsoleProgram {
+	public void run() {
+		int n = readInt();
+		println(fibonachi(n));
+	}
+	private int fibonachi(int n) {
+		int a = 1;
+		int b = 2;
+		int answer = 0;
+		for(int i = 3; i < n; i++) {
+			answer = a + b;
+			a = b;
+			b = answer;
+		}
+		return answer;
+	}
 }
