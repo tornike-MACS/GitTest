688b3bc4fe88aec48abb46aa3ef098ab3c7da96b
S <s>
2023-10-31 16:20:37 +0400

    {"types":"C","runs":8}

--- problem32.java
+++ problem32.java
@@ -4,21 +4,29 @@ import acm.program.ConsoleProgram;
 public class problem32 extends ConsoleProgram {
 	public void run() {
 		for(int i = 2; i <= 100000; i++) {
-			if(numberOfDivisors(i) == 2) {
+			if(isPrime(i)) {
 				println(i);
 			}
 		}
 	}
-	private int numberOfDivisors(int i) {
-		int divisors = 0;
-		for(int j = 1; j <= i; j++) {
-			if(i % j == 0) {
-				divisors++;
-			}
-			if(divisors > 2) {
-				break;
-			}
+	private boolean isPrime(int n) {
+		if(n == 2) return true;
+		if(n % 2 == 0)return false;
+		for(int i = 3; i * i < n; i += 2) {
+			if(n % i == 0) return false;
 		}
-		return divisors;
+		return true;
 	}
+//	private int numberOfDivisors(int i) {
+//		int divisors = 0;
+//		for(int j = 1; j <= i; j++) {
+//			if(i % j == 0) {
+//				divisors++;
+//			}
+//			if(divisors > 2) {
+//				break;
+//			}
+//		}
+//		return divisors;
+//	}
 }
