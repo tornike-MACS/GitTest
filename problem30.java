74f79bc28d77641d8a4d8a9fd38180925f62c569
S <s>
2023-11-04 19:06:00 +0400

    {"types":"C","runs":2}

--- problem30.java
+++ problem30.java
@@ -4,23 +4,29 @@ import acm.program.*;
 //შებრუნებით, 1256 -> 6521
 public class problem30 extends ConsoleProgram {
 	public void run() {
-		int answer = 0;
+//		int answer = 0;
+		int digit;
 		int n = readInt();
-		int digits = countDigits(n);
-		for(int i = digits; i > 0; i--) {
-			int a = n % 10;
-			answer += (int)Math.pow(10,i - 1) * a;
-			n = n / 10;
+		while(n > 0) {
+			digit = n % 10;
+			n /= 10;
+			print(digit);
 		}
-		println(answer);
+//		int digits = countDigits(n);
+//		for(int i = digits; i > 0; i--) {
+//			int a = n % 10;
+//			answer += (int)Math.pow(10,i - 1) * a;
+//			n = n / 10;
+		}
+//		println(answer);
 	}
 	
-	private int countDigits(int n) {
-		int digits = 0;
-		while(n > 0) {
-			n = n / 10;
-			digits++;
-		}
-		return digits;
-	}
-}
+//	private int countDigits(int n) {
+//		int digits = 0;
+//		while(n > 0) {
+//			n = n / 10;
+//			digits++;
+//		}
+//		return digits;
+	
+
