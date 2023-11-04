1ad532a9d85b83340ce181948f07464d2c0f0294
S <s>
2023-11-04 19:50:39 +0400

    {"types":"C","runs":32}

--- problem36.java
+++ problem36.java
@@ -5,6 +5,21 @@ import acm.util.*;
 //რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო
 public class problem36 extends ConsoleProgram{
 	public void run() {
-	
+		int bcount = 0;
+		int b3count = 0;
+		int count = 0;
+		for(int i = 0; i < 1000; i++) {
+			boolean b = rgen.nextBoolean();
+			if(b) {
+				bcount++;
+			}
+			if(bcount == 3) {
+				b3count++;
+				bcount = 0;
+			}
+			count++;
+		}
+		println(count / b3count);
 	}
+	RandomGenerator rgen = RandomGenerator.getInstance(); 
 }
