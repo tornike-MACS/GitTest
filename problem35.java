aaabbfad5a7f00aa889fc01391e83d4c8f3e75e1
S <s>
2023-11-04 19:28:11 +0400

    {"types":"C","runs":26}

--- problem35.java
+++ problem35.java
@@ -8,7 +8,7 @@ public class problem35 extends ConsoleProgram {
 	public void run() {
 		double bcount = 0;
 		double count = 0;
-		for(int i = 0; i < 1000; i++) {
+		for(int i = 0; i < 10000; i++) {
 			boolean b = rgen.nextBoolean();
 			if(b) {
 				bcount++;
