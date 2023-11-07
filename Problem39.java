09009d46ae7823b126fd4b14b303d6cff264a5a9
S <s>
2023-11-07 11:15:32 +0400

    {"types":"C","runs":2}

--- Problem39.java
+++ Problem39.java
@@ -4,16 +4,16 @@ import acm.program.GraphicsProgram;
 public class Problem39 extends GraphicsProgram{
 	public void run() {
 		int x = 1;
-		GRect rect = new GRect(x, getHeight() / 2 - 25, 50, 50);
+		GOval oval = new GOval(x, getHeight() / 2 - 25, 50, 50);
 		rect.setFilled(true);
 		add(rect);
 		while(true) {
-			rect.move(x, 0);
-			rect.pause(1);
-			if(rect.getX() == getWidth() - 50) {
+			oval.move(x, 0);
+			oval.pause(1);
+			if(oval.getX() == getWidth() - 50) {
 				x *= -1;
 			}
-			if(rect.getX() == 0) {
+			if(oval.getX() == 0) {
 				x *= -1;
 			}
 		}
