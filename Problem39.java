6f76912a89798129df06e48888eb1e5908eb09c1
S <s>
2023-11-07 11:30:29 +0400

    {"types":"C","runs":21}

--- Problem39.java
+++ Problem39.java
@@ -6,29 +6,16 @@ import acm.util.RandomGenerator;
 
 public class Problem39 extends GraphicsProgram{
 	public void run() {
-		int x = rgen.nextInt(1,2);
-		int y = rgen.nextInt(1, 2);
-		GOval oval = new GOval(x, getHeight() / 2 - 25, 50, 50);
+		GOval oval = new GOval(0, getHeight() / 2 - 25, 50, 50);
 		oval.setFilled(true);
 		oval.setColor(Color.RED);
 		add(oval);
 		while(true) {
-			oval.move(x, y);
-			oval.pause(2);
-			if(oval.getX() == getWidth() - 50) {
-				x *= -1;
-			}
-			if(oval.getX() == 0) {
-				x *= -1;
-			}
-			if(oval.getY() == getHeight()) {
-				y *= -1;
-			}
-			if(oval.getY() == 0) {
-				y *= -1;
+			oval.move(1, 0);
+			oval.pause(1);
+			if(oval.getX() == getWidth()) {
+				break;
 			}
 		}
 	}
-	RandomGenerator rgen = new RandomGenerator();
-	
 }
