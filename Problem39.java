fe6e7dcace501a5d0087efbe29f7e5da538e147d
S <s>
2023-11-07 11:21:58 +0400

    {"types":"C","runs":13}

--- Problem39.java
+++ Problem39.java
@@ -21,7 +21,7 @@ public class Problem39 extends GraphicsProgram{
 			if(oval.getX() == 0) {
 				x *= -1;
 			}
-			if(oval.getY() == getHeight() - 25) {
+			if(oval.getY() == getHeight()) {
 				y *= -1;
 			}
 			if(oval.getY() == 0) {
