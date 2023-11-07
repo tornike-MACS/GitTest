ca126746d64219da9699e62a9c4ff40b38a851b5
S <s>
2023-11-07 12:02:49 +0400

    {"types":"C","runs":30}

--- spinning.java
+++ spinning.java
@@ -5,7 +5,7 @@ import acm.program.GraphicsProgram;
 
 public class spinning extends GraphicsProgram {
 	public void run() {
-		GOval sun = new GOval(getWidth() / 2 - 50, getheight() / 2 - 50, 100, 100);
+		GOval sun = new GOval(getWidth() / 2 - 50, getHeight() / 2 - 50, 100, 100);
 		sun.setFilled(true);
 		sun.setColor(Color.YELLOW);
 		add(sun);
