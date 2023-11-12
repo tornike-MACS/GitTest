73916efa4eb232ac0b048ffef483750da484a646
S <s>
2023-11-12 19:42:39 +0400

    {"types":"C","runs":67}

--- Assignment3.java
+++ Assignment3.java
@@ -69,11 +69,11 @@ public class Assignment3 extends GraphicsProgram {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
+		addPoints();
 		addBricks();
 		addPaddle();
 		addMouseListeners();
 		addBall();
-		addPoints();
 	}
 
 private void addPoints() {
