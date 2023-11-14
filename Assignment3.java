edc7695981fa47055c42b6b90dd5fa2f1cbdac15
S <s>
2023-11-14 22:07:00 +0400

    {"types":"C","runs":56}

--- Assignment3.java
+++ Assignment3.java
@@ -76,7 +76,7 @@ public class Assignment3 extends GraphicsProgram {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
-		setBackground(Color.gray);
+		setBackground(Color.blue);
 		addBricks();
 		addTrialsLeft();
 		addPaddle();
