be291183abd0e42d3ff55486b5d3bfff61349b8c
S <s>
2023-11-16 20:52:06 +0400

    {"types":"C","runs":42}

--- Assignment3.java
+++ Assignment3.java
@@ -124,7 +124,7 @@ public class Assignment3 extends GraphicsProgram {
 			if(collider2 == paddle || collider4 == paddle) { // bottom points of the ball
 				ball.setLocation(ball.getX(), paddle.getY() - 2 * BALL_RADIUS);
 				vy = -vy;
-				bounceClip.play();
+				collideClip2.play();
 			}
 			if(collider1 != null && collider1 != paddle) {
 				remove(collider1);
