1e34895cb84c40f3fb6a51a9807af6765bb5512b
S <s>
2023-11-16 21:03:37 +0400

    {"types":"C","runs":53}

--- Assignment3.java
+++ Assignment3.java
@@ -189,7 +189,7 @@ public class Assignment3 extends GraphicsProgram {
 				gameOverClip1.play();
 				break;
 			}
-			if(countBricks == 10) {
+			if(countBricks == 100) {
 				GLabel WLabel = new GLabel("You won! :)");
 				WLabel.setFont("MONOSPACED-45");
 				WLabel.setColor(Color.MAGENTA);
