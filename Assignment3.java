df49c04acc0b2095f5b44807b70e5541c004bb60
S <s>
2023-11-16 19:49:16 +0400

    {"types":"C","runs":21}

--- Assignment3.java
+++ Assignment3.java
@@ -73,7 +73,7 @@ public class Assignment3 extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	AudioClip bounceClip = MediaTools.loadAudioClip("bounce.au");
-	AudioClip bounceClip1 = MediaTools.loadAudioClip("gameoversound.au");
+//	AudioClip bounceClip1 = MediaTools.loadAudioClip("gameoversound.au");
 
 	
 /* Method: run() */
