e45c91f72b5fa2f85b31475dab058f17185ed351
S <s>
2023-11-17 20:44:46 +0400

    {"types":"C","runs":25}

--- Assignment3.java
+++ Assignment3.java
@@ -131,10 +131,10 @@ public class Assignment3 extends GraphicsProgram {
 				if(collider2 == paddle || collider4 == paddle) { // bottom points of the ball
 					ball.setLocation(ball.getX(), paddle.getY() - 2 * BALL_RADIUS);
 					if(ball.getX() + BALL_RADIUS < paddle.getX() + PADDLE_WIDTH / 2 && vx >= 0) {
-						vx = - (paddle.getX() + PADDLE_WIDTH / 2 - ball.getX()) / 60 * vx;
+						vx = - (paddle.getX() + PADDLE_WIDTH / 2 - ball.getX()) / 30 * vx;
 					}
 					if(ball.getX() + BALL_RADIUS > paddle.getX() + PADDLE_WIDTH / 2 && vx < 0) {
-						vx = -(ball.getX() - (paddle.getX() + PADDLE_WIDTH / 2)) / 60 * vx;
+						vx = -(ball.getX() - (paddle.getX() + PADDLE_WIDTH / 2)) / 30 * vx;
 					}
 					vy = -vy;
 					bounceClip.play();
