cf6fdcb688df7998e6dc15ff7116119051fd5550
S <s>
2023-11-17 08:26:23 +0400

    {"types":"C","runs":59}

--- Problem44.java
+++ Problem44.java
@@ -6,12 +6,16 @@ import acm.program.GraphicsProgram;
 //4. მაუსის კლიკზე იმავე ადგილზე ჩნდება ბურთი და იწყებს ვარდნას ძირს, თუ მაუსს სხვა
 //ადგილზე ვაკლიკებ ბურთი გადადის ახალ ადგილზე და იქედან იწყებს ვარდნას
 public class Problem44 extends GraphicsProgram{
+	GOval ball;
 	public void run() {
 		addMouseListeners();
+		while(true) {
+			
+		}
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		GOval ball = new GOval(30,30);
+		ball = new GOval(30,30);
 		add(ball, e.getX(), e.getY());
 	}
 }
