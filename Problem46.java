01864c24af7dece51def165cff842eccef52e597
S <s>
2023-11-17 13:05:28 +0400

    {"types":"C","runs":22}

--- Problem46.java
+++ Problem46.java
@@ -4,9 +4,16 @@ import acm.program.ConsoleProgram;
 //ყველაზე ხშირ სიმბოლოს.
 public class Problem46 extends ConsoleProgram{
 	public void run() {
-		
+		String text = readLine("Enter text: ");
+		int maxCount = 0;
+		char maxCharacter = 0;
 		for (char c = 'a'; c <= 'z'; c++) {
-			println(c);
+			int count = countCharacters(text,c);
+			if(count > maxCount) {
+				maxCount = count;
+				maxCharacter = c;
+			}
 		}
+		println(maxCharacter);
 	}
 }
