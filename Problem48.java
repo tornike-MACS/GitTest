0ce8f674b35f90e0bcd96c12f09ee2521b2dfbf8
S <s>
2023-11-17 13:12:32 +0400

    {"types":"C","runs":24}

--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,8 @@
+import acm.program.ConsoleProgram;
 
-public class Problem48 {
+public class Problem48 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+	}
 
 }
