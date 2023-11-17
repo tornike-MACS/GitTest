9cba52e48623d4160f855a753337bd8ba390f91e
S <s>
2023-11-17 08:35:51 +0400

    {"types":"C","runs":68}

--- Problem45.java
+++ Problem45.java
@@ -1,8 +1,9 @@
+import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
 //45. მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
 //რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
-public class Problem45 extends GraphicsProgram {
+public class Problem45 extends ConsoleProgram {
 	public void run() {
 		readLine("Enter the text: ");
 		readLine("Enter the symbol: ");
