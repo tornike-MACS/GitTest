7e802cbddf78ccae501fbccb1c09aeb7b42908d2
S <s>
2023-11-18 20:29:54 +0400

    {"types":"C","runs":7}

--- Assignment3.java
+++ Assignment3.java
@@ -72,6 +72,8 @@ public class Assignment3 extends GraphicsProgram {
 	int countTrials = 0;
 	int countBricks = 0;
 	
+	GOval ball;
+	
 	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
@@ -125,7 +127,7 @@ public class Assignment3 extends GraphicsProgram {
 	}
 	
 	private void addBall() {
-		GOval ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+		ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
 		ball.setFilled(true);
 		ball.setColor(Color.MAGENTA);
 		add(ball, getWidth() / 2 - BALL_RADIUS, getHeight() / 2 - BALL_RADIUS); 
