c024e06d2573dd12eb87df572be763a4a73242f2
S <s>
2023-11-19 12:22:38 +0400

    {"types":"C","runs":24}

--- Problem45.java
+++ Problem45.java
@@ -5,21 +5,29 @@ import acm.program.GraphicsProgram;
 //რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 public class Problem45 extends ConsoleProgram {
 	public void run() {
-		int count = 0;
-		String text = readLine("Enter the text: ");
-		String sSymbol = readLine("Enter the symbol: ");
-		char symbol = sSymbol.charAt(0);
-		count = countChar(text, symbol);
-		println(count);
+		String text = readLine("Enter text: ");
+		String Ssymbol = readLine("Enter symbol: ");
+//		int count = 0;
+//		String text = readLine("Enter the text: ");
+//		String sSymbol = readLine("Enter the symbol: ");
+//		char symbol = sSymbol.charAt(0);
+//		count = countChar(text, symbol);
+//		println(count);
+//	}
+//	
+//	private int countChar(String text, char symbol) {
+//		int count = 0;
+//		for(int i = 0; i < text.length(); i++) {
+//			if(symbol == text.charAt(i)) {
+//				count++;
+//			}
+//		}
+//		return count;
 	}
 	
-	private int countChar(String text, char symbol) {
-		int count = 0;
-		for(int i = 0; i < text.length(); i++) {
-			if(symbol == text.charAt(i)) {
-				count++;
-			}
+	private int countChar(String text, string symbol) {
+		if(symbol.getLength() != 1) {
+			return false;
 		}
-		return count;
 	}
 }
