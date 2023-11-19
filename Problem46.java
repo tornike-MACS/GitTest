6ca3332504877f3b2f488a2e236d132b787787a9
S <s>
2023-11-19 12:27:22 +0400

    {"types":"C","runs":26}

--- Problem46.java
+++ Problem46.java
@@ -5,26 +5,31 @@ import acm.program.ConsoleProgram;
 public class Problem46 extends ConsoleProgram{
 	public void run() {
 		String text = readLine("Enter text: ");
-		int maxCount = 0;
-		char maxCharacter = 0;
-		for (char c = 'a'; c <= 'z'; c++) {
-			int count = countChar(text,c);
-			if(count > maxCount) {
-				maxCount = count;
-				maxCharacter = c;
-			}
-		}
-		println(maxCharacter);
-	}
+		
+		
+		
 	
-	private int countChar(String text, char symbol) {
-		int count = 0;
-		for(int i = 0; i < text.length(); i++) {
-			if(symbol == text.charAt(i)) {
-				count++;
-			}
-		}
-		return count;
+//		String text = readLine("Enter text: ");
+//		int maxCount = 0;
+//		char maxCharacter = 0;
+//		for (char c = 'a'; c <= 'z'; c++) {
+//			int count = countChar(text,c);
+//			if(count > maxCount) {
+//				maxCount = count;
+//				maxCharacter = c;
+//			}
+//		}
+//		println(maxCharacter);
+//	}
+//	
+//	private int countChar(String text, char symbol) {
+//		int count = 0;
+//		for(int i = 0; i < text.length(); i++) {
+//			if(symbol == text.charAt(i)) {
+//				count++;
+//			}
+//		}
+//		return count;
 	}
 }
 //49
