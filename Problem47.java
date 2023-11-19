e49b90878aa1f51f081c91179bc6f2a95c536084
S <s>
2023-11-19 13:27:20 +0400

    {"types":"C","runs":52}

--- Problem47.java
+++ Problem47.java
@@ -4,10 +4,16 @@ import acm.program.ConsoleProgram;
 //არა მოცემული ტექსტი პალინდრომი
 public class Problem47 extends ConsoleProgram{
 	public void run() {
+		int count = 0;
 		String text = readLine("Enter text: ");
 		for(int i = 0; i < text.length() / 2; i++) {
 			for(int j = text.length() - 1; j > text.length() / 2; j--) {
 				if(text.charAt(i) == text.charAt(j)) {
+					count++;
+					if(count == text.length()) {
+						println("palindrom");
+						break;
+					}
 					continue;
 				}else {
 					println("not palindrom");
