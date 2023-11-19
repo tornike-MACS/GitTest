1c694c2e83591a8e640bb862240c47db654c87dd
S <s>
2023-11-19 13:24:12 +0400

    {"types":"C","runs":49}

--- Problem47.java
+++ Problem47.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem47 {
-
+//47. დაწერეთ პრედიკატ მეთოდი რომელსაც გადაეცემა სტრინგი და რომელიც დაადგენს არის თუ
+//არა მოცემული ტექსტი პალინდრომი
+public class Problem47 extends ConsoleProgram{
+	public void run() {
+		String text = readLine("Enter text: ");
+		for(int i = 0; i < text.length() / 2; i++) {
+			for(int j = text.length() - 1; j > text.length() / 2; j--) {
+				if(text.charAt(i) == text.charAt(j)) {
+					continue;
+				}else {
+					println("not palindrom");
+					break;
+				}
+			}
+		}
+	}
 }
