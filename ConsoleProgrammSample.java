65b17a350d27e72992786f20a89887bfdce86e97
S <s>
2023-11-21 11:41:54 +0400

    {"types":"C","runs":4}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -4,15 +4,22 @@ import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-		println("This program creates the table of celsious to fahrenheit convention ");
-		for(double c = FROM; c<= TO; c += STEP) {
-			println(c + "C = " + celsiousToFahrenheit(c) + "F");
+		String number = readLine("enter number");
+		if(isDivisibleByThree(number)) {
+			println("divisible");
+		}else {
+			println("not");
 		}
 	}
-	private double celsiousToFahrenheit(double c){
-		return 9.0 / 5.0 * c + 32;
+	
+	private boolean isDivisibleByThree(String number) {
+		int sumOfDigits = 0;
+		for(int i = 0; i < number.length(); i++) {
+			sumOfDigits += number.charAt(i) - '0';
+		}
+		if(sumOfDigits % 3 ==0) {
+			return true;
+		}
+		return false;
 	}
-	private static final int FROM = 0;
-	private static final int TO = 100;
-	private static final int STEP = 5;
 }
