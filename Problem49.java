2f4e565c9cfbd6ecc027a6bb06330f291aa2d874
S <s>
2023-11-21 11:31:10 +0400

    {"types":"C","runs":1}

--- Problem49.java
+++ Problem49.java
@@ -7,10 +7,15 @@ import acm.program.ConsoleProgram;
 public class Problem49 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("enter number: ");
-		double n = stringToInteger(text);
+		int n = stringToInteger(text);
 		println(n);
 	}
-	private double stringToInteger(String text) {
-		
+	private int stringToInteger(String text) {
+		int number = 0;
+		for(int i = 0; i < text.length(); i++) {
+			int digit = text.charAt(i) - '0';
+			number = number * 10 + digit;
+		}
+		return number;
 	}
 }
