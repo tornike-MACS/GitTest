ba19aefceb9b7681c9f9c57b0fb3dc01cd920852
S <s>
2023-11-21 11:31:20 +0400

    {"types":"C","runs":2}

--- StringTokenizer.java
+++ StringTokenizer.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class StringTokenizer {
-
+public class StringTokenizer extends ConsoleProgram{
+	public void run() {
+		StringTokenizer tokenizer = new StringTokenizer("sdevsd");
+		int countWords = 0;
+		while (tokenizer.hasMoreTokens()) {
+			countWords++;
+			String token = tokenizer.nextToken();
+		}
+		println(countWords)
+	}
 }
