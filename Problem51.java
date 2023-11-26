5d184f81b468104b16491e53dd94eda609b33b88
S <s>
2023-11-26 16:16:54 +0400

    {"types":"C","runs":1}

--- Problem51.java
+++ Problem51.java
@@ -3,6 +3,7 @@ import acm.program.ConsoleProgram;
 //51. დაწერეთ მეთოდი რომელიც გადაცემული ტექსტიდან შლის დუბლირებულ სიმბოლოებს,
 //ანუ თუ ტექსტიში რომელიმე სიმბოლო მეორდება, უნდა დარჩეს მხოლოდ ყველაზე მარცხენა.
 public class Problem51 extends ConsoleProgram {
+	String letterSaver = "";
 	public void run() {
 		String text = readLine("Enter text: ");
 		String answer = removeDoubleWords(text);
@@ -10,12 +11,18 @@ public class Problem51 extends ConsoleProgram {
 	}
 	
 	private String removeDoubleWords(String text) {
-		String answer = " ";
+		letterSaver = saveLetters(text);
+//		String compressedWord = printCompressedWord();
+		return letterSaver;
+	}
+	
+	private String saveLetters(String text) {
 		for(int i = 0; i < text.length(); i++) {
-			if(answer.charAt(answer.length() - 1) != text.charAt(i)) {
-				answer += text.charAt(i);
+			if(letterSaver.contains(text.charAt(i) + "") == false) {
+				letterSaver += text.charAt(i);
 			}
 		}
-		return answer
+		return letterSaver;
 	}
-//}არასწორიააა contains ით
+	
+}
