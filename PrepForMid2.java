63d074b6ce5dc6c239bb884ce530f7661e389c04
S <s>
2023-11-28 12:03:18 +0400

    {"types":"C","runs":16}

--- PrepForMid2.java
+++ PrepForMid2.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class PrepForMid2 {
+public class PrepForMid2 extends ConsoleProgram{
+	public void run() {
+		int n = readInt();
+		if(oddEven(n)) {
+			println("true");
+		}else {
+			println("true");
+		}
+	}
 
+	private boolean oddEven(int n) {
+		while(n > 0) {
+			
+		}
+	}
 }
+//18181838 eseti unda iyos
