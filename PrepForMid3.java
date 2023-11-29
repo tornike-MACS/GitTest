06590540e5c84d72c6397a385cb705677ba82efe
S <s>
2023-11-29 15:44:04 +0400

    {"types":"C","runs":1}

--- PrepForMid3.java
+++ PrepForMid3.java
@@ -1,4 +1,15 @@
+import java.awt.event.MouseEvent;
 
-public class PrepForMid3 {
+import acm.program.GraphicsProgram;
 
+public class PrepForMid3 extends GraphicsProgram{
+	private int RADIUS = 30;
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		
+	}
 }
+//დაკლიკება წრე
