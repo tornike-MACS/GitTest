00ba1f01871793c8c42d6e7cc8bd077d351028f2
S <s>
2023-12-01 08:37:59 +0400

    {"types":"C","runs":68}

--- mid2021.java
+++ mid2021.java
@@ -40,8 +40,8 @@ public class mid2021 extends GraphicsProgram{
 	}
 	
 	public void mouseDragged(MouseEvent e) {
-//		x1 = x2;
-//		y1 = y2;
+		x1 = x2;
+		y1 = y2;
 		y2 = e.getY();
 		x2 = e.getX();
 		x1 = x2;
