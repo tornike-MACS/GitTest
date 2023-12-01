01177fd1d74590ac7f6796d7b56a837dda0a781c
S <s>
2023-12-01 20:39:00 +0400

    {"types":"C","runs":116}

--- sityvebisTamashi2017.java
+++ sityvebisTamashi2017.java
@@ -18,8 +18,8 @@ public class sityvebisTamashi2017 extends ConsoleProgram{
 	}
 	
 	private boolean spellWord(String a, String b) {
-		for(int i = 0; i < a.length() - 1; i++) {
-			for(int j = 1; j < a.length(); j++) {
+		for(int i = 0; i < a.length() - 2; i++) {
+			for(int j = 1; j < a.length() - 1; j++) {
 				String racxa = a.substring(i, j) + a.substring(j);
 				if(racxa.equals(b)) {
 					return true;
