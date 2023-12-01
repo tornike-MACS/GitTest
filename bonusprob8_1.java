e3fdfc206105e91005df301495dbd726c7836f9c
S <s>
2023-12-01 12:11:17 +0400

    {"types":"C","runs":8}

--- bonusprob8_1.java
+++ bonusprob8_1.java
@@ -5,16 +5,7 @@ import acm.program.*;
 
 public class bonusprob8_1 extends ConsoleProgram {
 	public void run() {
-		
+		println("acs" + 'a');
 	}
-	private boolean contains(String a, String b) {
-		int prevIndex = 0;
-		for(int i = 0; i < b.length(); i++) {
-			int index = a.indexOf(b.charAt(i), prevIndex);
-			if(index == -1) {
-				return false;
-			}
-		}
-		return true;
-	}
-//თავიდააან
+	
+}
