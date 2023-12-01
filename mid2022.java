adecf28fd2d6acdba17b02b223a3c7422974ab83
S <s>
2023-12-01 12:29:11 +0400

    {"types":"C","runs":18}

--- mid2022.java
+++ mid2022.java
@@ -1,4 +1,24 @@
+import java.awt.event.MouseEvent;
 
-public class mid2022 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+// gaferadeba ujrebis
+public class mid2022 extends GraphicsProgram{
+	
+	public void run() {
+		addMouseListeners();
+		double a = getWidth() / 8;
+		double b = getHeight() / 8;
+		for(int i = 0; i < 8; i ++) {
+			for(int j = 0; j < 8; j++) {
+				GRect rect = new GRect(a, b);
+				add(rect, a * i; b * j);
+			}
+		}
+	}
+	public void mouseClicked(MouseEvent e) {
+		
+	}
 }
+//or cvladshi vinaxavt gashavebulebs
