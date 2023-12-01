ae8230fba1a4094157addf01c41debf9d32f1c27
S <s>
2023-12-01 15:11:28 +0400

    {"types":"C","runs":72}

--- mid2017.java
+++ mid2017.java
@@ -1,4 +1,21 @@
+import java.awt.event.MouseEvent;
 
-public class mid2017 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+//ტაბლო
 
+public class mid2017 extends GraphicsProgram{
+	
+	private static final double FRAME_SIZE = 200;
+	
+	GRect frame; 
+	GLabel points;
+	public void run() {
+		frame = new GRect(FRAME_SIZE, FRAME_SIZE);
+		add(frame, getWidth() / 2 - FRAME_SIZE / 2, getHeight() / 2 - FRAME_SIZE / 2);
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		
+	}
 }
