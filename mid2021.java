6dce79f544938952d561692464beccfcee6416da
S <s>
2023-12-01 07:23:48 +0400

    {"types":"C","runs":1}

--- mid2021.java
+++ mid2021.java
@@ -1,4 +1,22 @@
-
-public class mid2021 {
-
+import acm.program.ConsoleProgram;
+//თქვენი ამოცანაა დაწეროთ მეთოდი რომელიც ლოგარითმს გამოითვლის. შეგახსენებთ რომ ლოგარითმი n-ის ფუძით m არის რიცხვი რომელ ხარისხშიც
+//უნდა ავიყვანოთ n იმისათვის რომ m მივიღოთ. ანუ, თუ n^x =m , მაშინ xის მთელი
+//ნაწილია შედეგი. n ^ x > m
+//log(5 ,25) =2
+//5, 26
+public class mid2021 extends ConsoleProgram{
+	public void run() {
+		int n = readInt();
+		int m = readInt();
+		println(log(n, m));
+	}
+	
+	private int log(int n, int m) {
+		int ans = 0;
+		while(m > 1) {
+			m /= n;
+			ans++;
+		}
+		return ans;
+	}
 }
