ae6083e6b59e307035b828a5325a8651b54f092c
S <s>
2023-12-01 09:18:38 +0400

    {"types":"C","runs":87}

--- mid2019.java
+++ mid2019.java
@@ -1,4 +1,26 @@
-
-public class mid2019 {
-
+import acm.program.ConsoleProgram;
+//palindrom
+public class mid2019 extends ConsoleProgram{
+	public void run() {
+		int n = readInt();
+		println(isPalindrom(n));
+	}
+	
+	private boolean isPalindrom(int n) {
+		if(reversedN(n) == n) {
+			return true;
+		}
+		return false;
+	}
+	private int reversedN(int n ) {
+		int lastDigit;
+		int revN = n % 10;
+		while(n > 0) {
+			revN = revN * 10;
+			n /= 10;
+			revN += n % 10;
+			n /= 10;
+		}
+		return revN;
+	}
 }
