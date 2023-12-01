c7c559b852e9deea5a4a1c1dd66764f384b37bc9
S <s>
2023-12-01 15:33:33 +0400

    {"types":"C","runs":77}

--- mid2017.java
+++ mid2017.java
@@ -17,13 +17,33 @@ public class mid2017 extends GraphicsProgram{
 	public void run() {
 		frame = new GRect(FRAME_SIZE, FRAME_SIZE);
 		add(frame, getWidth() / 2 - FRAME_SIZE / 2, getHeight() / 2 - FRAME_SIZE / 2);
-		points = new GLabel(intPoints + "");
-		add(points, getWidth() / 2 - points.getWidth() / 2, getHeight() / 2);
+		Gpoints();
+
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		x1 = e.getX();
 	}
 	
 	public void mouseDragged(MouseEvent e) {
-		
+		x2 = e.getX();
 	}
 	
+	public void mouseReleased(MouseEvent e) {
+		if(x1 < x2) {
+			intPoints++;
+			remove(points);
+			Gpoints();
+		}else {
+			intPoints--;
+			remove(points);
+			Gpoints();
+		}
+	}
 	
+	private void Gpoints () {
+		points = new GLabel(intPoints + "");
+		add(points, getWidth() / 2 - points.getWidth() / 2, getHeight() / 2);
+	}
+ 	
 }
