59340186f10c4b1e9edc8aca7e66bcc1d9b633e8
S <s>
2023-12-01 21:46:56 +0400

    {"types":"C","runs":155}

--- bonusprob8_1.java
+++ bonusprob8_1.java
@@ -1,11 +1,26 @@
-import java.awt.*;
-
-import acm.graphics.*;
-import acm.program.*;
+import acm.graphics.GOval;
+import acm.program.ConsoleProgram;
 
 public class bonusprob8_1 extends ConsoleProgram {
 	public void run() {
-		println('a' + 'b');
+		int x = 2;
+		int y = 4;
+		GOval o = new GOval(4, 2); // width, height
+		println(racxa(x, 2, o));
+		println(racxa(y, x / 2 + x, o));
 	}
-	
+
+	public int racxa(int x, int y, GOval o){
+		String s = "hello world";
+		x = varesiRacxa(x/y, x+(int)o.getHeight(), s);
+		o.scale(2);
+		y = varesiRacxa(x, y+(int)o.getHeight(), s);
+		return x+y;
+	}
+
+	public int varesiRacxa(double x, int y, String s) {
+		int z = (int) x + s.charAt(0) - s.charAt(1);
+		s = s.substring(1);
+		return z + y;
+	}
 }
