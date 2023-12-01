3768f565c81f8479b61166a402329963cebbe7a3
S <s>
2023-12-01 07:54:14 +0400

    {"types":"C","runs":34}

--- mid2021.java
+++ mid2021.java
@@ -1,23 +1,60 @@
 import acm.program.ConsoleProgram;
+//SPOILER ALERT! თქვენი ამოცანაა დაადგინოთ არის თუ არა ლუკა ანაკინის შვილი.
+//ამისათვის თქვენ გადმოგეცემათ ლუკას და ანაკინის DNA ნიმუშები (ორი სტრინგი). ეს ნიმუშები ერთმანეთს უნდა შეადაროთ და იპოვოთ მაქსიმალური სიგრძის
+//ზუსტი დამთხვევა რომელიც ორივე DNA-ს ნიმუშში გვხვდება. თუკი მაქსიმალური
+//სიგრძის დამთხვევის სიგრძე(სიმბოლოების რაოდენობა) მეტია ანაკინის DNA-ს
+//სიგრძის ნახევარზე მაშინ ლუკა ანაკინის შვილია წინააღმდეგ შემთხვევაში არ
+//არის. დაწერეთ მეთოდი amIYourFatherLuke რომელსაც გადაეცემა ორი სტრინგი და
+//აბრუნებს boolean-ს.ანუ თქვენ უნდა იპოვოთ ორ სტრინგში მაქსიმალური სიგრძის
+//ქვესტრინგი, რომელსაც ორივე სტრინგი შეიცავს. მაგალითად:
+//სტრინგებში "CGAT", "CCTGACT" მაქსიმალური სიგრძის ქვესტინგია "GA" სტრინგებში "GATTA", "ATTGATTT" 
+//მაქსიმალური სიგრძის ქვესტინგია "GATT" სტრინგებში "ATAGGCC", "ATCCGCCAG" მაქსიმალური სიგრძის ქვესტინგია "GCC"
+
+public class mid2021 extends ConsoleProgram{
+	public void run() {
+		String father = readLine();
+		String son = readLine();
+		println(amIYourFatherLuke(father, son));
+	}
+
+	private boolean amIYourFatherLuke(String father, String son) {
+		for(int i = 0; i < father.length(); i++) {
+			for(int j = 1; j < father.length(); j++) {
+				String subS = father.substring(i, j);
+				if(son.equals(subS)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
+
+
+
+
+
+
+
+
 //თქვენი ამოცანაა დაწეროთ მეთოდი რომელიც ლოგარითმს გამოითვლის. შეგახსენებთ რომ ლოგარითმი n-ის ფუძით m არის რიცხვი რომელ ხარისხშიც
 //უნდა ავიყვანოთ n იმისათვის რომ m მივიღოთ. ანუ, თუ n^x =m , მაშინ xის მთელი
 //ნაწილია შედეგი. n ^ x > m
 //log(5 ,25) =2
 //5, 26
-public class mid2021 extends ConsoleProgram{
-	public void run() {
-		int n = readInt();
-		int m = readInt();
-		println(log(n, m));
-	}
-	
-	private int log(int n, int m) {
-		int nextN = n;
-		int ans = 0;
-		while(m >= nextN) {
-			nextN *= n;
-			ans++;
-		}
-		return ans;
-	}
-}
+//public void run() {
+//int n = readInt();
+//int m = readInt();
+//println(log(n, m));
+//}
+//
+//private int log(int n, int m) {
+//int nextN = n;
+//int ans = 0;
+//while(m >= nextN) {
+//	nextN *= n;
+//	ans++;
+//}
+//return ans;
+//}
