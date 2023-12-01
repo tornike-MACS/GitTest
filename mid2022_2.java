b6ff6ec0b8f3c5c3e291bae0e9133cf12ef6529f
S <s>
2023-12-01 12:59:48 +0400

    {"types":"C","runs":23}

--- mid2022_2.java
+++ mid2022_2.java
@@ -13,8 +13,11 @@ public class mid2022_2 extends GraphicsProgram{
 	private RandomGenerator rg = RandomGenerator.getInstance();
 	private boolean isMoving;
 	
-	public void run() {
+	public void init() {
 		addMouseListeners();
+	}
+	
+	public void run() {
 		ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
 		ball.setFilled(true);
 		add(ball, getWidth() / 2 - BALL_RADIUS, 0);
