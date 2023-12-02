9a099548639d45a0ff16db4d1c83a90ad604b133
S <s>
2023-12-02 09:23:37 +0400

    {"types":"C","runs":1}

--- midProblem.java
+++ midProblem.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class midProblem {
-
+public class midProblem extends ConsoleProgram {
+	public void run() {
+		String s = readLine();
+		String ans = getTransformed(s);
+		println(ans);
+	}
+	
+	private String getTransformed(String s) {
+		int count = 0;
+		String ans = "";
+		for(int i = 0; i < s.length() - 1; i++) {
+			if(s.charAt(i) == s.charAt(i + 1)) {
+				count++;
+			}else {
+				ans += "" + count + s.charAt(i);
+			}
+		}
+		return ans;
+	}
 }
