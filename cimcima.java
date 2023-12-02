9003aca716f55b99c35fdab88b898e69a101b40d
S <s>
2023-12-02 09:59:45 +0400

    {"types":"C","runs":11}

--- cimcima.java
+++ cimcima.java
@@ -1,4 +1,25 @@
+import java.awt.event.MouseEvent;
 
-public class cimcima {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+
+
+public class cimcima extends GraphicsProgram {
+	private GOval ball;
+	
+	private static final double SIZE = 40;
+	
+	public void run() {
+		addMouseListeners();
+		
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		ball = (GOval)getElementAt(e.getX(), e.getY());
+		if(ball == null) {
+			ball = new GOval(SIZE, SIZE);
+			add(ball, e.getX() - SIZE / 2, e.getY() - SIZE / 2);
+		}
+	}
 }
