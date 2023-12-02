ba538101d5a82b728c5ffcef7592275646a6ff55
S <s>
2023-12-02 14:45:27 +0400

    {"types":"C","runs":132}

--- mid2021.java
+++ mid2021.java
@@ -1,8 +1,60 @@
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GLine;
+import acm.graphics.GOval;
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
+/*-
+ * დაწერეთ გრაფიკული პროგრამა, რომელსაც აქვს ორი რეჟიმი: ამატებს ან კვადრატს, ან წრეს.
+ * 
+ * 1. მაუსის კლიკზე ან კვადრატი ემატება ფანჯარაზე, ან წრე (იმ წერტილზე სადაც კლიკი მოხდა)
+ * 2. რეჟიმის ცვლილება ხდება მაუსის დრაგის მეშვეობით. თუ მომხმარებელმა გააკეთა დრაგი:
+ *    - მარჯვნიდან->მარცხნივ: შემდეგ კლიკზე ემატება ოვალი  
+ *    - მარცხნიდან->მარჯვნივ: შემდეგ კლიკზე ემატება კვადრატი
+ * 3. თავიდან სანამ დრეგი არ მოხდება (default-ად) ემატება ოვალი
+ */
+public class mid2021 extends GraphicsProgram{
+	
+	private boolean ovalOrRect = false;
+	
+	private int x1;
+	private int x2;
+	
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		if(!ovalOrRect) {
+			GOval oval = new GOval(30, 30);
+			add(oval, e.getX() - 15, e.getY() - 15);
+		}else {
+			GRect rect = new GRect(30, 30);
+			add(rect, e.getX() - 15, e.getY() - 15);
+		}
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		if(x1 < x2) {
+			ovalOrRect = true;
+		}else {
+			ovalOrRect = false;
+		}
+	}
+	
+	public void mousePressd(MouseEvent e) {
+		x1 = e.getX();
+	}
+	public void mouseReleased(MouseEvent e) {
+		x2 = e.getX();
+	}
+	
+}
+
+
+
+
 
 
 
@@ -13,23 +65,21 @@ import acm.program.GraphicsProgram;
 //ნაწილია შედეგი. n ^ x > m
 //log(5 ,25) =2
 //5, 26
-public class mid2021 extends ConsoleProgram{
-	public void run() {
-		int n = readInt();
-		int m = readInt();
-		println(log(n, m));
-	}
-		
-	private int log(int n, int m) {
-		int count = 0;
-		int prev = n;
-		while(prev <= m) {
-			prev = prev * n;
-			count++;
-		}
-		return count;
-	}
-}
+//public void run() {
+//	int n = readInt();
+//	int m = readInt();
+//	println(log(n, m));
+//}
+//	
+//private int log(int n, int m) {
+//	int count = 0;
+//	int prev = n;
+//	while(prev <= m) {
+//		prev = prev * n;
+//		count++;
+//	}
+//	return count;
+//}
 ////SPOILER ALERT! თქვენი ამოცანაა დაადგინოთ არის თუ არა ლუკა ანაკინის შვილი.
 ////ამისათვის თქვენ გადმოგეცემათ ლუკას და ანაკინის DNA ნიმუშები (ორი სტრინგი). ეს ნიმუშები ერთმანეთს უნდა შეადაროთ და იპოვოთ მაქსიმალური სიგრძის
 ////ზუსტი დამთხვევა რომელიც ორივე DNA-ს ნიმუშში გვხვდება. თუკი მაქსიმალური
