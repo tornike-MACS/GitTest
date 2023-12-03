fefc3187b6b5e2adca75fafad725e38ed6b0afcc
S <s>
2023-12-03 21:26:55 +0400

    {"types":"C","runs":17}

--- randomBalls.java
+++ randomBalls.java
@@ -11,12 +11,12 @@ public class randomBalls extends GraphicsProgram{
 	
 	public void run() {
 		addMouseListeners();
-		for(int i = 0; i < 20; i++) {
-			double radius = rg.nextDouble(10, 30);
-			GOval oval = new GOval(2 * radius, 2 * radius);
-			oval.setFilled(true);
-			add(oval, rg.nextDouble(0, getWidth() - 2 * radius), rg.nextDouble(0, getHeight() - 2 * radius));
-		}
+//		for(int i = 0; i < 20; i++) {
+//			double radius = rg.nextDouble(10, 30);
+//			GOval oval = new GOval(2 * radius, 2 * radius);
+//			oval.setFilled(true);
+//			add(oval, rg.nextDouble(0, getWidth() - 2 * radius), rg.nextDouble(0, getHeight() - 2 * radius));
+//		}
 	}
 	
 	public void mouseClicked(MouseEvent e) {
