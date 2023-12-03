c92b9c653069fa629205846697a3f6503a4e5a49
S <s>
2023-12-03 21:21:48 +0400

    {"types":"C","runs":7}

--- randomBalls.java
+++ randomBalls.java
@@ -21,6 +21,7 @@ public class randomBalls extends GraphicsProgram{
 	
 	public void mouseClicked(MouseEvent e) {
 		if(getElementAt(e.getX(), e.getY()) != null) {
+			System.out.println("sd");
 			ball = (GOval)getElementAt(e.getX(), e.getY());
 			ball.setColor(rg.nextColor());
 		}
