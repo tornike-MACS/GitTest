8916577e0fff1288b7e3f5d890bc5e171cf56ad1
S <s>
2023-12-03 21:24:44 +0400

    {"types":"C","runs":13}

--- randomBalls.java
+++ randomBalls.java
@@ -13,14 +13,14 @@ public class randomBalls extends GraphicsProgram{
 		addMouseListeners();
 		for(int i = 0; i < 20; i++) {
 			double radius = rg.nextDouble(10, 30);
-			ball = new GOval(2 * radius, 2 * radius);
-			ball.setFilled(true);
-			add(ball, rg.nextDouble(0, getWidth() - 2 * radius), rg.nextDouble(0, getHeight() - 2 * radius));
+			GOval oval = new GOval(2 * radius, 2 * radius);
+			oval.setFilled(true);
+			add(oval, rg.nextDouble(0, getWidth() - 2 * radius), rg.nextDouble(0, getHeight() - 2 * radius));
 		}
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		if(getElementAt(e.getX(), e.getY()) == ball) {
+		if(getElementAt(e.getX(), e.getY()) != null) {
 			System.out.println("sd");
 			ball = (GOval)getElementAt(e.getX(), e.getY());
 			ball.setColor(rg.nextColor());
