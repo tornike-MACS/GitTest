3281352de5799f9431be324e618694108ddb54d6
S <s>
2023-12-03 21:16:02 +0400

    {"types":"C","runs":1}

--- randomBalls.java
+++ randomBalls.java
@@ -1,4 +1,18 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class randomBalls {
 
+public class randomBalls extends GraphicsProgram{
+	
+	private GOval ball;
+	private RandomGenerator rg = RandomGenerator.getInstance();
+	
+	public void run() {
+		for(int i = 0; i < 20; i++) {
+			double radius = rg.nextDouble(10, 30);
+			GOval oval = new GOval(2 * radius, 2 * radius);
+			add(oval);
+		}
+	}
 }
