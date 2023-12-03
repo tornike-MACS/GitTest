ee35d2216166ad04ead447e5eb5113550c5113bb
S <s>
2023-12-03 21:38:40 +0400

    {"types":"C","runs":25}

--- evalMid.java
+++ evalMid.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class evalMid {
-
+public class evalMid extends ConsoleProgram{
+	public void run() {
+		String s = readLine();
+		println(eval(s));
+	}
+	
+	private int eval(String s) {
+		
+		return 0;
+	}
 }
