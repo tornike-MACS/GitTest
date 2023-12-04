486d983f44b1b654c337bb59ea2302bcb01cfd1d
S <s>
2023-12-04 20:38:10 +0400

    {"types":"C","runs":1}

--- randomBalls.java
+++ randomBalls.java
@@ -19,11 +19,13 @@ public class randomBalls extends GraphicsProgram{
 			oval.setFilled(true);
 			add(oval, rg.nextDouble(0, getWidth() - 2 * radius), rg.nextDouble(0, getHeight() - 2 * radius));
 		}
-//		while(startMoving) {
-//			System.out.println("daiwyo");
-//			ball.move(0, 3);
-//			pause(1);
-//		}
+		while(true) {
+			System.out.println("daiwyo");
+			if(startMoving) {
+				ball.move(0, 3);
+			}
+			pause(1);
+		}
 	}
 	
 	public void mouseClicked(MouseEvent e) {
@@ -32,12 +34,7 @@ public class randomBalls extends GraphicsProgram{
 			ball.setColor(rg.nextColor());
 		}else{
 			System.out.println("sdc");
-//			startMoving = true;
-			while(true) {
-				System.out.println("daiwyo");
-				ball.move(0, 3);
-				pause(1);
-			}
+			startMoving = true;
 		}
 	}
 }
