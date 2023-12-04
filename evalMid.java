45c85c0822c1d0b28381b5a7a5f9aaa64449bb73
S <s>
2023-12-04 20:58:43 +0400

    {"types":"C","runs":4}

--- evalMid.java
+++ evalMid.java
@@ -3,17 +3,31 @@ import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
 public class evalMid extends ConsoleProgram{
+	
+	private boolean plus = true;
+	
 	public void run() {
-//		String s = readLine();
-		StringTokenizer st = new StringTokenizer("sd ef ", " ");
+		int ans = 0;
+		String s = readLine();
+		StringTokenizer st = new StringTokenizer(s, " ");
 		while(st.hasMoreTokens()) {
-			println(st.nextToken());
+			if(st.nextToken().equals("+")) {
+				plus = true;
+			}else if(st.nextToken().equals("-")) {
+				plus = false;
+			}else{
+				if(plus) {
+					ans += Integer.parseInt(st.nextToken());
+				}else {
+					ans -= Integer.parseInt(st.nextToken());
+				}
+			}
 		}
-//		println(eval(s));
+		println(ans);
 	}
 	
-	private int eval(String s) {
-		
-		return 01;
-	}
+//	private int eval(String s) {
+//		
+//		return 01;
+//	}
 }
