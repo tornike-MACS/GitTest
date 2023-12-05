9299d402fb1acb69b6d4732db2069f3e1fb21181
S <s>
2023-12-05 11:39:11 +0400

    {"types":"C","runs":2}

--- arrays.java
+++ arrays.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class arrays {
-
+public class arrays extends ConsoleProgram{
+	public void run() {
+		int[] numbers = new int[10];
+		go(numbers);
+		println(numbers[0]);
+	}
+	private void go(int[] numbers) {
+		numbers[0] = 15;
+	}
 }
