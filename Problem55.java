acf53dff0203f491acceaae937158888f014cc0c
S <s>
2023-12-05 12:01:55 +0400

    {"types":"C","runs":8}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem55 {
-
+public class Problem55 extends ConsoleProgram{
+	public void run() {
+		int n  = readInt();
+		int[] number = new int[n];
+		for(int i = 0; i < n - 1; i ++) {
+			number[i] = readInt();
+			if (number[i] > number[i + 1]) {
+				println("not");
+				break;
+			}
+		}
+		println("yes");
+	}
 }
