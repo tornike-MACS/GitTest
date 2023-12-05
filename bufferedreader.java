4b1233dced29aae51d64f01a3ddb4fb68b413316
S <s>
2023-12-05 11:39:10 +0400

    {"types":"C","runs":2}

--- bufferedreader.java
+++ bufferedreader.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class bufferedreader {
+import acm.program.ConsoleProgram;
 
+public class bufferedreader extends ConsoleProgram{
+	public void run() {
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(""));
+			while(true) {
+				String line = reader.readLine();
+				if(line == null) {
+					break;
+				}
+			}
+		}catch(FileNotFoundException e) {
+			println("File not found");
+		}catch(IOException e) {
+			println("not opened");
+		}
+	}
 }
