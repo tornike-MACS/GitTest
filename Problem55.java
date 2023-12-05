6fc69b470697fc76d1af7896cdb291751b4de3a6
S <s>
2023-12-05 12:06:21 +0400

    {"types":"C","runs":15}

--- Problem55.java
+++ Problem55.java
@@ -5,7 +5,7 @@ public class Problem55 extends ConsoleProgram{
 		boolean yes = true;
 		int n  = readInt();
 		int[] number = new int[n];
-		for(int i = 1; i < n; i ++) {
+		for(int i = 1; i <= n; i ++) {
 			number[i] = readInt();
 			if (number[i - 1] > number[i]) {
 				yes = false;
