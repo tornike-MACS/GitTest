75afab29d3f95133cafd34d79e1e706d13ea3eda
S <s>
2023-12-05 12:06:55 +0400

    {"types":"C","runs":15}

--- Problem55.java
+++ Problem55.java
@@ -2,14 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Problem55 extends ConsoleProgram{
 	public void run() {
-		boolean yes = true;
 		int n  = readInt();
 		int[] number = new int[n];
 		for(int i = 1; i <= n; i ++) {
 			number[i] = readInt();
-			if (number[i - 1] > number[i]) {
-				yes = false;
-			}
 		}
 		println(yes);
 	}
