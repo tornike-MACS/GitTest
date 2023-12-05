6e5c774b86ef2d17f2822295141bba8875a46354
S <s>
2023-12-05 11:51:04 +0400

    {"types":"C","runs":5}

--- Problem56.java
+++ Problem56.java
@@ -1,4 +1,17 @@
+import java.util.Arrays;
 
-public class Problem56 {
+import acm.program.ConsoleProgram;
 
-}
+public class Problem56 extends ConsoleProgram{
+	public void run() {
+		int n = readInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = readInt();
+		}
+		Arrays.sort(arr);
+		for(int i = 0; i < n; i++) {
+			println(arr[i]);
+		}
+	}
+}	
