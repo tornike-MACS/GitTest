090c45a2e40543424a1c68dd415d229ff238f1b6
S <s>
2023-12-08 13:18:38 +0400

    {"types":"C","runs":18}

--- Problem57.java
+++ Problem57.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 //57. დაწერეთ მეთოდი, რომელსაც გადაეცემა ორი სტრინგი და აბრუნებს true-ს თუკი ეს ორის
@@ -8,6 +10,20 @@ import acm.program.ConsoleProgram;
 //შემდეგ შეადარეთ ერთმანეთს მასივები.
 public class Problem57 extends ConsoleProgram{
 	public void run() {
-		
+		String first = readLine();
+		String second = readLine();
+		println(isAnagram(first, second));
+	}
+	
+	private int[] getFrequencies(String string) {
+		int[] frequencies = new int[26];
+		for(int i = 0; i < string.length(); i++) {
+			frequencies[string.charAt(i) - 'a'] ++;
+		}
+		return frequencies;
+	}
+	
+	private boolean isAnagram(String first, String second) {
+		return Arrays.equals(getFrequencies(first), getFrequencies(second));
 	}
 }
