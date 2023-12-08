dc756060bad43cb7f5bda81c599eed03f6c136b2
S <s>
2023-12-08 12:47:08 +0400

    {"types":"C","runs":6}

--- Problem60.java
+++ Problem60.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
-public class Problem60 {
+import acm.program.ConsoleProgram;
 
+public class Problem60 extends ConsoleProgram{
+	public void run() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while(true) {
+			int n = readInt();
+			if(n == -1) {
+				break;
+			}
+			list.add(n);
+		}
+		for(int i = 0; i < list.size(); i ++) {
+			println(list.get(list.size() - i));
+		}
+	}
 }
