88c1b4df34d8e75c5e3b674007a57ef8a051b536
S <s>
2023-12-08 13:10:47 +0400

    {"types":"C","runs":17}

--- Problem60.java
+++ Problem60.java
@@ -6,21 +6,25 @@ public class Problem60 extends ConsoleProgram{
 	public void run() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		
-		while(true) {
-			int n = readInt();
-			if(n == -1) {
-				break;
-			}
-			list.add(0, n);
-		}
-		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i) == 1) {
-				list.remove(i);
-				i--;
-			}
-		}
+		while(list.remove((Integer)1));//1 ianebs shlis
 		println(list);
+		
+//		--------------
+//		while(true) {
+//			int n = readInt();
+//			if(n == -1) {
+//				break;
+//			}
+//			list.add(0, n);
+//		}
+//		for(int i = 0; i < list.size(); i++) {
+//			if(list.get(i) == 1) {
+//				list.remove(i);
+//				i--;
+//			}
+//		}
+//		println(list);
+//		---------------
 ////		for(int i = list.size() - 1; i >= 0; i --) {
 ////			println(list.get(i));
 ////		}
