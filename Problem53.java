cb98e328101b7cfddf16bf4c40fa9f23279b50d6
S <s>
2023-12-08 08:47:06 +0400

    {"types":"C","runs":3}

--- Problem53.java
+++ Problem53.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem53 {
+import acm.program.ConsoleProgram;
 
+//53. დაწერეთ პროგრამა რომელიც ფაილიდან წაიკითხავს ტექტს და დათვლის შემდეგ
+//სტატისტიკებს: სიმბოლოების რაოდენობა, სიტყვების რაოდენობა(სიტყვები სფეისებით
+//გამოყოფილია ერთმანეთისგან), წინადადებების რაოდენობა. რამდენი წინდადება მთავრდება
+//წერტილით, კითხვის ნიშნით და ძახილის ნიშნით?
+public class Problem53 extends ConsoleProgram{
+	public void run() {
+		int count = 0;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Problem53.java"));
+			while(true) {
+				int a = reader.read();
+				if(a == 1) {
+					break;
+				}
+				count++;
+			}
+		}catch(IOException e) {
+			
+		}
+		println(count);
+	}
 }
