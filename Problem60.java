8aef3423def9acedfcc0e48819eaca7d68d28fbb
S <s>
2023-12-08 12:48:48 +0400

    {"types":"C","runs":8}

--- Problem60.java
+++ Problem60.java
@@ -12,8 +12,8 @@ public class Problem60 extends ConsoleProgram{
 			}
 			list.add(n);
 		}
-		for(int i = 0; i < list.size(); i ++) {
-			println(list.get(list.size() - i));
+		for(int i = list.size(); i > 0; i --) {
+			println(list.get(i));
 		}
 	}
 }
