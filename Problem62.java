ab6798bec3b8ff3a75d60a0786b57984e8a63094
S <s>
2023-12-09 20:09:40 +0400

    {"types":"C","runs":14}

--- Problem62.java
+++ Problem62.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Problem62 {
-
+import acm.program.ConsoleProgram;
+//62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს. დაბეჭდეთ
+//სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.
+public class Problem62 extends ConsoleProgram{
+	public void run() {
+		ArrayList<String> list = new ArrayList<String>();
+		while(true) {
+			String s = readLine();
+			if(s == "")break;
+			list.add(s);
+		}
+		for(int i = 0; i < list.size(); i++) {
+			println(list.get(i) + countReapetedWords(list, list.get(i)));
+		}
+	}
+	
+	private int countReapetedWords(ArrayList<String> list, String s) {
+		int count = 0;
+		for(int i = 0; i < list.size(); i++) {
+			if(s.equals(list.get(i))) {
+				count++;
+			}
+		}
+		return count;
+	}
 }
