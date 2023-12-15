78b8eb4eafebb5eb331dfdcad7ba6d1bc403d247
S <s>
2023-12-15 13:14:23 +0400

    {"types":"C","runs":1}

--- Problem61.java
+++ Problem61.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 import acm.program.ConsoleProgram;
 
 //61. კონსოლიდან შეგვყავს რიცხვი m და n, შემდეგ n ცალი რიცხვი. პროგრამამ უნდა იპოვოს
@@ -6,16 +8,30 @@ public class Problem61 extends ConsoleProgram{
 	public void run() {
 		int m = readInt();
 		int n = readInt();
-		int[] arr = new int[n];
+		HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 		for(int i = 0; i < n; i++) {
-			arr[i] = readInt();
-		}
-		for(int i = 0; i < n; i++) {
-			for(int j = i + 1; j < n - i; j++) {
-				if(arr[i] + arr[j] == m) {
-					println(arr[i] + " " + arr[j]);
-				}
+			int key = readInt();
+			hashMap.put(key, i);
+			if(hashMap.get(m - key) == 0) {
+				println(hashMap.get(m - key) + "" + hashMap.get(key));
 			}
 		}
+		
+		
+		
+		
+		
+		
+//		int[] arr = new int[n];
+//		for(int i = 0; i < n; i++) {
+//			arr[i] = readInt();
+//		}
+//		for(int i = 0; i < n; i++) {
+//			for(int j = i + 1; j < n - i; j++) {
+//				if(arr[i] + arr[j] == m) {
+//					println(arr[i] + " " + arr[j]);
+//				}
+//			}
+//		}
 	}
 }
