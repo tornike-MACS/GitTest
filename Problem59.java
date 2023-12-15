f257ba633b0719efff01205f8fbaa3e09d56664b
S <s>
2023-12-15 09:20:10 +0400

    {"types":"C","runs":11}

--- Problem59.java
+++ Problem59.java
@@ -20,8 +20,8 @@ public class Problem59 extends ConsoleProgram{
 	public void run() {
 		int[][] matrix = new int[3][3];
 		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				matrix[i][j] = 0;
+			for(int j = 0; j < 4; j++) {
+				matrix[i][j] = 1;
 			}
 		}
 		
