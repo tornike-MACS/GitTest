fa15fccc982dd7be34c9ee214d9480fc79517c76
S <s>
2023-12-15 12:36:53 +0400

    {"types":"C","runs":1}

--- hashMapPractice.java
+++ hashMapPractice.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
 
-public class hashMapPractice {
+import acm.program.ConsoleProgram;
 
+public class hashMapPractice extends ConsoleProgram{
+	public void run() {
+		HashMap <String, Integer> hashMap = new HashMap<String, Integer>();
+		
+		hashMap.put("abc", 2000);
+		int y = hashMap.get("abc");
+		
+		for(String x : hashMap.keySet()) {
+			println(x + " " + hashMap.get(x));
+		}
+		
+		
+	}
 }
