16808536a11159b82238ad48b1f437c61d22d983
S <s>
2023-12-15 08:50:19 +0400

    {"types":"C","runs":1}

--- Problem58.java
+++ Problem58.java
@@ -11,16 +11,29 @@ import acm.program.ConsoleProgram;
 public class Problem58 extends ConsoleProgram{
 	public void run() {
 		int[][] arr = new int[5][5];
-		println(magicMatrix());
+		println(magicMatrix(arr));
 	}
 	
 	private boolean magicMatrix(int[][] array) {
 		int n = array.length;
-		for(int i = 0; i < n * n; i++) {
-			
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int[] arr = new int[n * n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				list.add(array[i][j]);
+			}
 		}
+		for(int i = 0; i < n * n; i++) {
+			arr[i] = list.get(i);
+		}
+		Arrays.sort(arr);
+		if( arr[0] == 1 && arr[n * n] == n * n) {
+			return true;
+		}
+		return false;
 		
 	}
+}
 
 
 
