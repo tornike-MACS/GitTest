40cfb0eea97a2831a7c822a524a8648225c9306e
S <s>
2023-12-15 13:14:25 +0400

    {"types":"C","runs":1}

--- hashMapPractice.java
+++ hashMapPractice.java
@@ -1,18 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
 public class hashMapPractice extends ConsoleProgram{
 	public void run() {
-		HashMap <String, Integer> hashMap = new HashMap<String, Integer>();
+		HashMap <String, ArrayList<Integer>> hashMap = new HashMap<String, Integer>();
+		ArrayList <Integer> arrList = new ArrayList<Integer>();
 		
-		hashMap.put("abc", 2000);
-		int y = hashMap.get("abc");
+//		hashMap.put("abc", 2000);
+//		int y = hashMap.get("abc");
+		
+		hashMap.put("abcd", arrList);
 		
 		for(String x : hashMap.keySet()) {
 			println(x + " " + hashMap.get(x));
 		}
 		
+		hashMap.putIfAbsent(key, value);
+		hashMap.getOrDefault(key, null);// null is magivrad sxva ramec sheidzleba
 		
 	}
 }
