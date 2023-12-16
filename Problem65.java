cc4e10627c0a3c1b932ff3f5a2ab9e2ad5224597
S <s>
2023-12-16 15:49:53 +0400

    {"types":"C","runs":94}

--- Problem65.java
+++ Problem65.java
@@ -1,4 +1,16 @@
+import java.util.HashMap;
 
-public class Problem65 {
-
+import acm.program.ConsoleProgram;
+//65. 63-ე ამოცანის გაგრძელება. დაბეჭდეთ ადამიანი რომელსაც ყველაზე მეტი ადამიანი
+//ემეგობრება
+public class Problem65 extends ConsoleProgram{
+	public void run() {
+		HashMap<String, String> map = new HashMap<String,String>();
+		while(true) {
+			String a = readLine("A: ");
+			if(a.length() == 0)break;
+			String b = readLine("B: ");
+			
+		}
+	}
 }
