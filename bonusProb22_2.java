b359103d224f068757d5688ab0f27c3e8614c18e
S <s>
2023-12-16 16:48:48 +0400

    {"types":"C","runs":109}

--- bonusProb22_2.java
+++ bonusProb22_2.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class bonusProb22_2 {
-
+//2. დაწერეთ მეთოდი რომელსაც არგუმენტად გადმოეცემა int-ების მატრიცა და ამ
+//მატრიცას ატრიალებს სიმეტრიულად y ღერძის მიმართ1, 2 -> 2,1
+//3, 4 4,3
+public class bonusProb22_2 extends ConsoleProgram{
+	public void run() {
+		int[][] matrix = new int[2][3];
+		println(reverseMatrix(matrix));
+	}
+	
+	private int[][] reverseMatrix(int[][] matrix) {
+		
+	}
 }
