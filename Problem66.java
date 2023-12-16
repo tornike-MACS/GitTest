f445a0698e9d0187ed5a6ed1e1fe37a2fd45aa81
S <s>
2023-12-16 16:13:03 +0400

    {"types":"C","runs":98}

--- Problem66.java
+++ Problem66.java
@@ -1,4 +1,23 @@
+import java.util.HashMap;
 
-public class Problem66 {
+import acm.program.ConsoleProgram;
 
+//66. 63-ე ამოცანის გაგრძელება. დაბეჭდეთ ორი ადამინი რომელთაც არ ჰყავთ საერთო მეგობრები
+public class Problem66 extends ConsoleProgram{
+	public void run() {
+		HashMap<String, String> map = new HashMap<String, String>();
+		while(true) {
+			String ans1 = "";
+			String ans2 = "";
+			String a = readLine("A: ");
+			if(a.length() == 0)break;
+			String b = readLine("B: ");
+			if(map.containsKey(a) == false) {
+				map.put(a, b);
+			}else {
+				
+			}
+			
+		}
+	}
 }
