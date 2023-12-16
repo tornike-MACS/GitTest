d5217fb1ed65bc2a1ccfd438b531bb38867b11da
S <s>
2023-12-16 17:02:15 +0400

    {"types":"C","runs":114}

--- bonusProb22_2.java
+++ bonusProb22_2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 //2. დაწერეთ მეთოდი რომელსაც არგუმენტად გადმოეცემა int-ების მატრიცა და ამ
@@ -6,10 +8,22 @@ import acm.program.ConsoleProgram;
 public class bonusProb22_2 extends ConsoleProgram{
 	public void run() {
 		int[][] matrix = new int[2][3];
-		println(reverseMatrix(matrix));
+		matrix[0][0] = 1;
+		matrix[0][1] = 2;
+		matrix[0][2] = 3;
+		matrix[0][0] = 1;
+		matrix[0][1] = 2;
+		matrix[0][2] = 3;
+		println(Arrays.deepToString(reverseMatrix(matrix)));
 	}
 	
 	private int[][] reverseMatrix(int[][] matrix) {
-		
+		int[][] copy = Arrays.copyOf(matrix, matrix.length);
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				copy[i][j] = matrix[i][matrix[i].length - 1 - j]; 
+			}
+		}
+		return copy;
 	}
 }
