4a5d2574be59f8148eed86f160a9ca7a5840076e
S <s>
2023-12-16 16:49:39 +0400

    {"types":"C","runs":111}

--- bonusProb22.java
+++ bonusProb22.java
@@ -33,7 +33,7 @@ public class bonusProb22 extends ConsoleProgram{
 		}
 		for(int i = 0; i < matrix.length; i++) {
 //			for(int j = 0; j < matrix[0].length; j++) {
-				println(matrix[i]);
+				println(matrix[i].toString());
 //			}
 		}
 	}
