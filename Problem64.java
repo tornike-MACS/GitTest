5ce646b88c031b42808a099d5e2802850844f5ed
S <s>
2023-12-16 14:44:28 +0400

    {"types":"C","runs":73}

--- Problem64.java
+++ Problem64.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Problem64 {
+import acm.program.ConsoleProgram;
 
-}
+//64. 63-ე ამოცანის გაგრძელება, დაბეჭდეთ თითოეული შეყვანილი სახელი და მის გასწვრივ
+//მძიმით გამოყოფილი მეგობრების სია.
+public class Problem64 extends ConsoleProgram{
+	public void run() {
+		HashMap<String, String> map = new HashMap<String, String>();
+		ArrayList<String> list = new ArrayList<String>();
+		while(true) {
+		String a = readLine("A: ");
+		if(list.contains(a) == false) {
+			list.add(a);
+		}
+		if(a.length() == 0)break;
+		String b = readLine("B: ");
+		if(list.contains(b) == false) {
+			list.add(b);
+		}
+		map.put(a, b);
+		}
+		
+		for(int i = 0; i < list.size(); i++) {
+			println(list.get(i) + friendsList(list.get(i), map));
+		}
+	}
+	
+	private ArrayList<String> friendsList(String s, HashMap<String, String> map) {
+		ArrayList<String> ans = new ArrayList<String>();
+		for(String x : map.keySet()) {
+			if(map.get(x) == s) {
+				ans.add(map.get(x));
+			}
+		}
+		return ans;
+	}
+}		
