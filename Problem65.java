8d526855423e94a7de4e5d9186b761a7a4a7599f
S <s>
2023-12-16 15:57:10 +0400

    {"types":"C","runs":97}

--- Problem65.java
+++ Problem65.java
@@ -6,11 +6,29 @@ import acm.program.ConsoleProgram;
 public class Problem65 extends ConsoleProgram{
 	public void run() {
 		HashMap<String, String> map = new HashMap<String,String>();
+		int count = 0;
+		int max = 0;
+		String ans = "";
 		while(true) {
 			String a = readLine("A: ");
 			if(a.length() == 0)break;
 			String b = readLine("B: ");
-			
+			map.put(a, b);
+			count = countFriends(map, b);
+			if(count > max) {
+				max = count;
+				ans = b;
+			}
 		}
+		println(ans);
+	}
+	private int countFriends(HashMap<String,String> map, String s) {
+		int count = 0;
+		for(String x : map.keySet()) {
+			if(map.get(x).equals(s)) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
