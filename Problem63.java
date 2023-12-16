59ca7575b669f73eea6e65b9b61c1af81e12765d
S <s>
2023-12-16 14:21:17 +0400

    {"types":"C","runs":70}

--- Problem63.java
+++ Problem63.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 import acm.program.ConsoleProgram;
 //63. კონსოლიდან შეგვყავს წყვილ-წყვილად მეგობრების სახელები, მანამ სანამ არ შევიყვანთ
 //ცარიელ სტრინგს. დაბეჭდეთ იმ ადამიანის სახელი ვისაც ჰყავს ყველაზე მეტი მეგობარი.
@@ -5,6 +7,31 @@ import acm.program.ConsoleProgram;
 //B-ს მეგობარი, მაგრამ B შეიძლება არ იყოს A-ს მეგობარი.
 public class Problem63 extends ConsoleProgram{
 	public void run() {
-		
+		HashMap<String, String> map = new HashMap<String, String>();
+		int count = 0;
+		int max = 0;
+		String ans = "";
+		while(true) {
+			String a = readLine("A: ");
+			if(a.length() == 0)break;
+			String b = readLine("B: ");
+			map.put(b, a);
+			count = countFriends(map, b);
+			if(count > max) {
+				max = count;
+				ans = b;
+			}
+		}
+		println(ans);
+	}
+	
+	private int countFriends(HashMap<String, String> map, String s) {
+		int count = 0;
+		for(String x : map.keySet()) {
+			if(map.get(x) == s) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
