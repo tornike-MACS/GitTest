74ef39c1ad2e51e3b3202d95965f7767d5ab3077
S <s>
2023-12-21 20:23:06 +0400

    {"types":"C","runs":50}

--- JcomponentExample.java
+++ JcomponentExample.java
@@ -1,4 +1,10 @@
+import javax.swing.JLabel;
 
-public class JcomponentExample {
+import acm.program.ConsoleProgram;
 
+public class JcomponentExample extends ConsoleProgram{
+	public void run() {
+		JLabel label = new JLabel("hi");
+		add(label, SOUTH);
+	}
 }
