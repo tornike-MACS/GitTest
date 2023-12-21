853e89e6e5dd645cc1ad0a36869fdadb40741dbd
S <s>
2023-12-21 20:54:15 +0400

    {"types":"C","runs":73}

--- JcomponentExample.java
+++ JcomponentExample.java
@@ -4,22 +4,25 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+import acm.graphics.GImage;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class JcomponentExample extends ConsoleProgram{
+public class JcomponentExample extends GraphicsProgram{
 	public void run() {
-		JLabel label = new JLabel("name");
-		add(label, SOUTH);
-		
-		JTextField field = new JTextField(10);
-		field.setText("");
-		field.addActionListener(this);
-		add(field, SOUTH);
-		
-		JButton button = new JButton("Press here!");
-		add(button, SOUTH);
-		
-		
+//		JLabel label = new JLabel("name");
+//		add(label, SOUTH);
+//		
+//		JTextField field = new JTextField(10);
+//		field.setText("");
+//		field.addActionListener(this);
+//		add(field, SOUTH);
+//		
+//		JButton button = new JButton("Press here!");
+//		add(button, SOUTH);
+//		
+		GImage image = new GImage("Screenshot 2023-11-16 211117.png");
+		add(image);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
