197aab1e67a81add82e1d439946cea54461c5d73
S <s>
2023-12-21 20:23:17 +0400

    {"types":"C","runs":51}

--- JcomponentExample.java
+++ JcomponentExample.java
@@ -5,6 +5,6 @@ import acm.program.ConsoleProgram;
 public class JcomponentExample extends ConsoleProgram{
 	public void run() {
 		JLabel label = new JLabel("hi");
-		add(label, SOUTH);
+		add(label, NORTH);
 	}
 }
