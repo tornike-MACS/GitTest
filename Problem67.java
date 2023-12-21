238e6c22ab6b976a14242d2c9eae985a07a73b70
S <s>
2023-12-21 20:55:25 +0400

    {"types":"C","runs":74}

--- Problem67.java
+++ Problem67.java
@@ -1,4 +1,9 @@
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
 
-public class Problem67 {
-
+public class Problem67 extends GraphicsProgram{
+	public void run() {
+		GImage image = new GImage("Screenshot 2023-11-16 211117.png");
+		add(image);
+	}
 }
