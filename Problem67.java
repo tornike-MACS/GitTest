113300493b3e284de879fbcd753cbe7bd15a2108
S <s>
2023-12-21 20:59:30 +0400

    {"types":"C","runs":75}

--- Problem67.java
+++ Problem67.java
@@ -1,9 +1,24 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTextField;
+
 import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
-
+//67. გააკეთეთ ფოტოების აპლიკაცია. აპლიკაციას უნდა ჰქონდეს ტექსფილდი სადაც შეიძლება
+//ფოტოს(ფაილის) მისამართის ჩაწერა. ენტერზე დაჭერის შემდეგ ეს ფოტო უნდა გამოჩნდეს
+//ფანჯრის ცენტრში.
 public class Problem67 extends GraphicsProgram{
 	public void run() {
-		GImage image = new GImage("Screenshot 2023-11-16 211117.png");
+		
+		JTextField field = new JTextField();
+		add(field, SOUTH);
+		String photo = field.getText();
+		
+		GImage image = new GImage(photo);
 		add(image);
 	}
+	
+//	public void actionPerformed(ActionEvent e) {
+//		println(e.getActionCommand());
+//	}
 }
