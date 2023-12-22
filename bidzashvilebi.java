de67bcdffa85ddfa11da4b6e86f339e04fc7d2f1
S <s>
2023-12-22 13:03:35 +0400

    {"types":"C","runs":7}

--- bidzashvilebi.java
+++ bidzashvilebi.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-public class bidzashvilebi {
+import acm.program.ConsoleProgram;
 
-}
+public class bidzashvilebi extends ConsoleProgram{
+	public void run() {
+		HashMap<String, ArrayList<String>> cousins = new HashMap<String, ArrayList<String>>();
+		HashMap<String, String> map = new HashMap<String, String>();
+		while(true) {
+			String line = readLine();
+			if(line.isEmpty()) {
+				break;
+			}
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			String a = tokenizer.nextToken();
+			String b = tokenizer.nextToken();
+			map.put(b, a);
+		}
+		
+		for(String n : map.keySet()) {
+			for(String m : map.keySet()) {
+				if(map.get(n).equals(map.get(m))) {
+					continue;
+				}
+				String dad1 = map.get(n);
+				String dad2 = map.get(m);
+				if(map.containsKey(dad1) && map.containsKey(dad2)) {
+					if(map.get(dad1).equals(map.get(dad2))) {
+						cousins.get(n).add(m);
+						cousins.get(m).add(n);
+					}
+				}
+			}
+		}
+		println(cousins);
+	}
+}	
