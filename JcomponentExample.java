ade08c1cbcfc89a4072b5c4536ef6c1226ad47d6
S <s>
2023-12-23 11:33:46 +0400

    {"types":"C","runs":28}

--- JcomponentExample.java
+++ JcomponentExample.java
@@ -9,23 +9,29 @@ import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
 public class JcomponentExample extends ConsoleProgram{
+	JTextField field;
+	JButton button;
+	
 	public void run() {
 //		JLabel label = new JLabel("name");
 //		add(label, SOUTH);
 //		
-		JTextField field = new JTextField(10);
+		field = new JTextField(10);
 		field.setText("");
-//		field.addActionListener(this);
+		field.addActionListener(this);
 		add(field, SOUTH);
 //		
-		JButton button = new JButton("Press here!");
+		button = new JButton("Press here!");
 		add(button, SOUTH);
 //		
 //		GImage image = new GImage("Screenshot 2023-11-16 211117.png");
 //		add(image, CENTER);
+		
+		addActionListeners();
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		if(field.action(button, e.getActionCommand()));
 		println(e.getActionCommand());
 	}
 }
