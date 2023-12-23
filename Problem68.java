d47107659d6b50e0d17cb764ef8028b11995926f
S <s>
2023-12-23 14:09:09 +0400

    {"types":"C","runs":3}

--- Problem68.java
+++ Problem68.java
@@ -31,7 +31,7 @@ public class Problem68 extends GraphicsProgram{
 		if(e.getActionCommand().equals("Enter")) {
 			text = new JLabel(field.getText());
 			add(text, 10,y);
-			y += 10;	
+			y += 15;	
 		}
 	}
 }
