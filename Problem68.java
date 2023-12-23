e56fc0e2b499d519ea47ee0dc2219983d5b63f64
S <s>
2023-12-23 12:29:27 +0400

    {"types":"C","runs":39}

--- Problem68.java
+++ Problem68.java
@@ -1,4 +1,16 @@
+import javax.swing.JTextField;
 
-public class Problem68 {
+import acm.program.GraphicsProgram;
 
+//68. თქვენი ამოცანაა გრაფიკული პროგრამის საშუალებით გააკეთოთ ჩატი. ფანჯარაზე, SOUTH
+//რეგიონზე უნდა გქონდეთ ერთი ტექსტფილდი და ერთი ღილაკი ტექტით Enter.
+//ტექსტფილდში ტექსტის შეყვანის მერე Enter ჭილაკზე დაჭერით ტექსტი უნდა
+//იწერებოდეს(იხატებოდეს) კანვასზე ხოლო ტექსტფილდში კი ტექსტი იშლებოდეს. ხელახლა
+//ტექსტის შეყვანისას ახალი ტექსტი წინა ტექსტის ქვემოთ უნდა გამოცნდეს(როგორც ჩატში
+//ხდება ხოლმე. თუკი ტექსტი ჩაცდება ეკრანს არაუშავს.
+public class Problem68 extends GraphicsProgram{
+	public void run() {
+		JTextField field = new JTextField(10);
+		add(field, SOUTH);
+	}
 }
