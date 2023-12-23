8498388dd4dc4fa051f461297f3d81d65378a1de
S <s>
2023-12-23 14:45:57 +0400

    {"types":"C","runs":13}

--- Problem69.java
+++ Problem69.java
@@ -1,4 +1,10 @@
+import acm.program.GraphicsProgram;
 
-public class Problem69 {
-
+//69. 68 ამოცანის გაგრძელება. დაამატეთ სქროლი. თუკი ტექსტი ჩაცდება ეკრანს მაშინ ახალი
+//ტექსტი მაინც უნდა გამოჩნდეს ეკრანის ბოლოში, შესაბამისად სულ პირველად შეყვანილი
+//ტექსტი გაქრება ეკრანიდან.
+public class Problem69 extends GraphicsProgram{
+	public void run() {
+		
+	}
 }
