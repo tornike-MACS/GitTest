e28d726deaf87fe73eac1cfe1884852fa768fee6
S <s>
2023-12-24 15:15:53 +0400

    {"types":"C","runs":10}

--- Problem66.java
+++ Problem66.java
@@ -27,29 +27,6 @@ public class Problem66 extends ConsoleProgram{
 			map.get(b).add(a);
 		}
 		
-		String ans1 = "";
-		String ans2 = "";
-		boolean doNotHaveMutual = true;
-		for(String x : map.keySet()) {
-			for(String z : map.keySet()) {
-				Iterator it = map.get(x).iterator();
-				while(it.hasNext()) {
-					if(map.get(z).contains(it.next())) {
-						doNotHaveMutual = false;
-						break;
-					}
-				}
-				if(doNotHaveMutual) {
-					ans1 = x;
-					ans2 = z;
-					break;
-				}
-			}
-			if(doNotHaveMutual) {
-				println(ans1 + " " + ans2);
-				break;
-			}
-		}
 	}
 }
 //martiviaa
