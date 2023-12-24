c3ed4c61b4995e46baac3dec1dd88aa89950e20d
S <s>
2023-12-24 14:24:37 +0400

    {"types":"C","runs":0}

--- Problem66.java
+++ Problem66.java
@@ -1,12 +1,22 @@
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
 //66. 63-ე ამოცანის გაგრძელება. დაბეჭდეთ ორი ადამინი რომელთაც არ ჰყავთ საერთო მეგობრები
 public class Problem66 extends ConsoleProgram{
 	public void run() {
+		String a;
+		String b;
 		HashMap<String, String> map = new HashMap<String, String>();
-		
+		while(true) {
+			StringTokenizer tk = new StringTokenizer(readLine());
+			a = tk.nextToken();
+			b = tk.nextToken();
+		break;
+			
+		}
+		println(a + b);
 	}
 }
 //martiviaa
