f8b1bcfd28c91ce31251295db671273941d9605f
S <s>
2023-12-24 16:38:12 +0400

    {"types":"C","runs":25}

--- Problem68.java
+++ Problem68.java
@@ -34,6 +34,11 @@ public class Problem68 extends GraphicsProgram{
 			add(text, 10,y);
 			field.setText(null);
 			y += 15;	
+		}else {
+			text = new JLabel(field.getText());
+			add(text, 10,y);
+			field.setText(null);
+			y += 15;
 		}
 	}
 }
