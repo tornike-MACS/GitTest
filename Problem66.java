815ddeb414d74e7eb32e05a4f9f20eb8423cfa3b
S <s>
2023-12-24 14:26:50 +0400

    {"types":"C","runs":4}

--- Problem66.java
+++ Problem66.java
@@ -6,14 +6,15 @@ import acm.program.ConsoleProgram;
 //66. 63-ე ამოცანის გაგრძელება. დაბეჭდეთ ორი ადამინი რომელთაც არ ჰყავთ საერთო მეგობრები
 public class Problem66 extends ConsoleProgram{
 	public void run() {
-		String a;
-		String b;
+		String a  = "";
+		String b = "";
 		HashMap<String, String> map = new HashMap<String, String>();
 		while(true) {
-			StringTokenizer tk = new StringTokenizer(readLine());
+			String line = readLine();
+			if(line.isEmpty())break;
+			StringTokenizer tk = new StringTokenizer(line);
 			a = tk.nextToken();
 			b = tk.nextToken();
-		break;
 			
 		}
 		println(a + b);
