3e8d7385fbba81ca339d65bb0bcece7d94cc34ab
S <s>
2023-12-24 16:32:28 +0400

    {"types":"C","runs":20}

--- atiDawyebulebisAmoshla.java
+++ atiDawyebulebisAmoshla.java
@@ -1,4 +1,26 @@
+import java.util.HashSet;
+import java.util.Iterator;
 
-public class atiDawyebulebisAmoshla {
+import acm.program.ConsoleProgram;
 
+public class atiDawyebulebisAmoshla extends ConsoleProgram{
+	public void run() {
+		HashSet<String> set = new HashSet<String>();
+		while(true) {
+			String line = readLine();
+			if(line.isEmpty())break;
+			set.add(line);
+		}
+		HashSet<String> ansSet = new HashSet<String>();
+		
+		Iterator<String> it = set.iterator();
+		
+		while(it.hasNext()) {
+			String a = it.next();
+			if(a.charAt(0) != 'a') {
+				ansSet.add(a);
+			}
+		}
+		println(ansSet);
+	}
 }
