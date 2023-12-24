64bfbf861d8c6a774aaf27a3b7edb58af60525dc
S <s>
2023-12-24 15:34:25 +0400

    {"types":"C","runs":12}

--- bidzashvilebi.java
+++ bidzashvilebi.java
@@ -7,38 +7,30 @@ import acm.program.ConsoleProgram;
 // rogor amovshalot a ti dawyebuli vtqvat hashsetidan
 public class bidzashvilebi extends ConsoleProgram{
 	public void run() {
-		HashMap<String, ArrayList<String>> cousins = new HashMap<String, ArrayList<String>>();
 		HashMap<String, String> map = new HashMap<String, String>();
+		String son = "";
+		String dad = "";
 		while(true) {
 			String line = readLine();
-			if(line.isEmpty()) {
-				break;
-			}
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			String a = tokenizer.nextToken();
-			String b = tokenizer.nextToken();
-			map.put(b, a);
+			if(line.isEmpty())break;
+			StringTokenizer tk = new StringTokenizer(line);
+			son = tk.nextToken();
+			dad = tk.nextToken();
+			map.put(son, dad);
 		}
-		
-		for(String n : map.keySet()) {
-			for(String m : map.keySet()) {
-				if(map.get(n).equals(map.get(m))) {
-					System.out.println("asc");
-					continue;
+		boolean found = false;
+		for(String x : map.keySet()) {
+			for(String y : map.keySet()) {
+				if(map.get(x).equals(map.get(y)))continue;;
+				String dad1 = map.get(x);
+				String dad2 = map.get(y);
+				if(map.get(dad1).equals(map.get(dad2))) {
+					println(x + " " + y);
+					found = true;
+					break;
 				}
-				String dad1 = map.get(n);
-				String dad2 = map.get(m);
-				if(map.containsKey(dad1) && map.containsKey(dad2)) {
-					System.out.println("asc");
-					if(map.get(dad1).equals(map.get(dad2))) {
-						cousins.putIfAbsent(n, new ArrayList<String>());
-						cousins.putIfAbsent(m, new ArrayList<String>());
-						cousins.get(n).add(m);
-						cousins.get(m).add(n);
-					}
-				
 			}
+			if(found)break;
 		}
-		println(cousins);
 	}
 }	
