20900f98ea52e53c9ebad8a0fdbad4319c8d9fa0
S <s>
2023-12-26 11:41:47 +0400

    {"types":"C","runs":0}

--- bonusWinner.java
+++ bonusWinner.java
@@ -1,4 +1,27 @@
+import java.util.*;
 
-public class bonusWinner {
+import acm.program.ConsoleProgram;
 
+public class bonusWinner extends ConsoleProgram{
+	public void run() {
+		
+	}
+	
+	private String bonusWinner(HashMap <String, Integer> results) {
+		HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+		for(int result : results.values()) {
+			frequencies.put(result, frequencies.getOrDefault(result,  0) + 1);
+		}
+		String winner = null;
+		for(String student : results.keySet()) {
+			int result = results.get(student);
+			if(frequencies.get(result) == 1) {
+				if(winner == null || results.get(winner) > result)  {
+					winner = student;
+				}
+			}
+		}
+		return null;
+	}
 }
+//!!!
