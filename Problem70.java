68102cba86140e3fcfe298b39b0cfcc5d1288321
S <s>
2023-12-29 08:23:11 +0400

    {"types":"C","runs":6}

--- Problem70.java
+++ Problem70.java
@@ -2,14 +2,13 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import acm.graphics.GLine;
-import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 //70. მთელ ფორმაზე ბადის დახატვა, ისე რომ ფორმის ზომის ცვლილებაზე ბადის უჯრის ზომაც
 //პროპორციულად იზრდებოდეს
 public class Problem70 extends GraphicsProgram{
 	public void run() {
 		drawNet();
-		addComponentListener(this);
+		addComponentListener((ComponentListener) this);
 	}
 	
 	private void drawNet() {
