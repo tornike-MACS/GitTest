a6d157c2feadac3ed8565ecb86dc40dde6657a53
S <s>
2023-12-29 08:16:26 +0400

    {"types":"C","runs":0}

--- Problem70.java
+++ Problem70.java
@@ -1,4 +1,20 @@
-
-public class Problem70 {
-
+import acm.graphics.GLine;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+//70. მთელ ფორმაზე ბადის დახატვა, ისე რომ ფორმის ზომის ცვლილებაზე ბადის უჯრის ზომაც
+//პროპორციულად იზრდებოდეს
+public class Problem70 extends GraphicsProgram{
+	public void run() {
+		drawNet();
+	}
+	
+	private void drawNet() {
+		removeAll();
+		for(int i = 0; i < 10; i++) {
+			GLine vLine = new GLine(i * getWidth() / 10, 0, i * getWidth() / 10, getHeight());
+			add(vLine);
+			GLine hLine = new GLine(0, i * getHeight() / 10, getWidth(), i * getHeight() / 10);
+			add(hLine);
+		}
+ 	}
 }
